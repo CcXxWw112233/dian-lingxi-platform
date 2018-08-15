6966ea9e0eed70da8472eb70220a955536344c3a
chenxuwei <1109567482@qq.com>
2018-08-15 11:49:13 +0800

    项目、界面

---
+++ src/routes/Technological/components/Project/ElseProject.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import indexStyle from './index.less'
+import globalStyles from '../../../../globalset/css/globalClassName.less'
+import { Icon, Menu, Dropdown, Tooltip, Collapse, Card, Modal } from 'antd'
+
+export default class CollectionProject extends React.Component{
+  state = {
+    starType: 'star-o',
+    starOpacity: 0.6
+  }
+
+  //星星样式变化start-------------------------
+  starMouseOver() {
+    if(this.state.starType === 'star'){
+      return false
+    }
+    this.setState({
+      starType: 'star-o',
+      starOpacity: 1
+    })
+  }
+  starMouseLeave() {
+    if(this.state.starType === 'star'){
+      return false
+    }
+    this.setState({
+      starType: 'star-o',
+      starOpacity: 0.6
+    })
+  }
+  starClick() {
+    this.setState({
+      starType: this.state.starType === 'star' ? 'star-o' : 'star',
+      starOpacity: 1
+    })
+  }
+  //星星样式变化end-------------------------
+ confirm() {
+    Modal.confirm({
+      title: 'Confirm',
+      content: <div>sss</div>,
+      okText: '确认',
+      cancelText: '取消',
+    });
+  }
+  handleMenuClick(e) {
+    const { key } = e
+    console.log(e)
+    if('4' === key) {
+      this.confirm()
+    }
+  }
+  render() {
+    const taskMan = [1,2,3,4,5,6,7,8]
+    const { starType,starOpacity } = this.state
+    const menu = (
+      <Menu onClick={this.handleMenuClick.bind(this)}>
+        <Menu.Item key={'1'}  style={{textAlign: 'center',padding:0,margin: 0}}>
+          <div className={indexStyle.elseProjectMemu}>
+            邀请成员加入
+          </div>
+        </Menu.Item>
+        <Menu.Item key={'2'} style={{textAlign: 'center',padding:0,margin: 0}}>
+          <div className={indexStyle.elseProjectMemu}>
+            项目归档
+          </div>
+        </Menu.Item>
+        <Menu.Item key={'3'}  style={{textAlign: 'center',padding:0,margin: 0}}>
+          <div className={indexStyle.elseProjectMemu}>
+            删除项目
+          </div>
+        </Menu.Item>
+        <Menu.Item key={'4'}  style={{textAlign: 'center',padding:0,margin: 0}}>
+          <div className={indexStyle.elseProjectDangerMenu}>
+            退出项目
+          </div>
+        </Menu.Item>
+      </Menu>
+    );
+    return (
+      <Card style={{position: 'relative',height: 'auto'}}>
+        <div className={indexStyle.listOutmask}></div>
+        <div className={indexStyle.listOut}>
+          <div className={indexStyle.left}>
+            <div className = {indexStyle.top}>
+              <span>[项目实例]关于切从未如此一目了然</span>
+              <span className={indexStyle.nameHoverMenu} >
+                <Icon className={indexStyle.star}
+                      onMouseOver={this.starMouseOver.bind(this)}
+                      onMouseLeave={this.starMouseLeave.bind(this)}
+                      onClick={this.starClick.bind(this)}
+                      type={starType} style={{margin: '0 0 0 8px',opacity: starOpacity,color: '#FAAD14 '}} />
+                  <Dropdown overlay={menu} >
+                    <Icon type="ellipsis"  style={{fontSize:18,margin: '0 0 0 8px'}}/>
+                  </Dropdown>
+              </span>
+            </div>
+            <div className ={indexStyle.bottom}>
+              {taskMan.map((value, key) => {
+                if(key < 7) {
+                  return (<img src="" key={key} className={indexStyle.taskManImag}></img>)
+                }
+              })}
+              {taskMan.length > 7? (
+                <div style={{display: 'flex',fontSize: 12}}>
+                  <div className={indexStyle.manwrap} ><Icon type="ellipsis" style={{fontSize:18}}/></div>{taskMan.length}位任务执行人
+                </div>
+              ) : ('')}
+            </div>
+          </div>
+          <div className={indexStyle.right}>
+            <div className={indexStyle.rightItem}>
+              <div>27</div>
+              <div>剩余任务</div>
+            </div>
+            <div className={indexStyle.rightItem}>
+              <div style={{color: '#8c8c8c'}}>27</div>
+              <div>已完成</div>
+            </div>
+            <div className={indexStyle.rightItem}>
+              <div >27</div>
+              <div>距离下一节点</div>
+            </div>
+          </div>
+        </div>
+      </Card>
+    )
+  }
+}
--- src/routes/Technological/components/Project/ProjectList.js
+++ src/routes/Technological/components/Project/ProjectList.js
@@ -2,49 +2,17 @@ import React from 'react'
 import indexStyle from './index.less'
 import globalStyles from '../../../../globalset/css/globalClassName.less'
 import { Icon, Menu, Dropdown, Tooltip, Collapse, Card } from 'antd'
+import CollectionProject from './CollectionProject'
+import ElseProject from './ElseProject'
+
 const Panel = Collapse.Panel
 
 export default class Projectlist extends React.Component {
 
+  projectNameMouseOVer() {
+    console.log(111)
+  }
   render() {
-    const taskMan = [1,2,3,4,5,6,7,8]
-    const collectionProject = (
-      <Card style={{position: 'relative',height: 'auto'}}>
-        <div className={indexStyle.listOutmask}></div>
-        <div className={indexStyle.listOut}>
-          <div className={indexStyle.left}>
-            <div className = {indexStyle.top}>[项目实例]关于工作的一切从未如此一目了然</div>
-            <div className ={indexStyle.bottom}>
-              {taskMan.map((value, key) => {
-                console.log(key)
-                if(key < 7) {
-                  return (<img src="" key={key} className={indexStyle.taskManImag}></img>)
-                }
-              })}
-              {taskMan.length > 7? (
-                <div style={{display: 'flex',fontSize: 12}}>
-                  <div className={indexStyle.manwrap} ><Icon type="ellipsis" style={{fontSize:18}}/></div>{taskMan.length}位任务执行人
-                </div>
-              ) : ('')}
-            </div>
-          </div>
-          <div className={indexStyle.right}>
-            <div className={indexStyle.rightItem}>
-              <div>27</div>
-              <div>剩余任务</div>
-            </div>
-            <div className={indexStyle.rightItem}>
-              <div>27</div>
-              <div>已完成</div>
-            </div>
-            <div className={indexStyle.rightItem}>
-              <div>27</div>
-              <div>距离下一节点</div>
-            </div>
-          </div>
-        </div>
-      </Card>
-    )
     const addItem = (
       <div className={indexStyle.addListItem}>
         <Icon type="plus-circle-o" style={{fontSize: 18, color: '#8c8c8c',marginTop: 6}} />
@@ -52,15 +20,16 @@ export default class Projectlist extends React.Component {
     )
     return (
       <div className={indexStyle.projectListOut}>
-        <Collapse accordion bordered={false} style={{backgroundColor:'#f5f5f5',marginTop: 30}} defaultActiveKey={['1']}>
-          <Panel header="This is panel header 1" key="1"  style={customPanelStyle}>
-            {collectionProject}
+        <Collapse accordion bordered={false} style={{backgroundColor:'#f5f5f5',marginTop: 30}} defaultActiveKey={['2']}>
+          <Panel header="我收藏的项目" key="1"  style={customPanelStyle}>
+             <CollectionProject/>
             {addItem}
           </Panel>
-          <Panel header="This is panel header 2" key="2"  style={customPanelStyle}>
-
+          <Panel header="我管理的项目" key="2"  style={customPanelStyle}>
+            <ElseProject/>
+            {addItem}
           </Panel>
-          <Panel header="This is panel header 3" key="3"  style={customPanelStyle}>
+          <Panel header="我参与的项目" key="3"  style={customPanelStyle}>
 
           </Panel>
         </Collapse>
