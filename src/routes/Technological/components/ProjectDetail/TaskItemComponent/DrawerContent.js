3fb62eb7a2df1a0378f63fd62a0b50dcec1273ea
chenxuwei <1109567482@qq.com>
2018-08-24 17:29:17 +0800

    富文本 添加标签

--- src/routes/Technological/components/ProjectDetail/TaskItemComponent/DrawerContent.js
+++ src/routes/Technological/components/ProjectDetail/TaskItemComponent/DrawerContent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import DrawerContentStyles from './DrawerContent.less'
-import { Icon } from 'antd'
+import { Icon, Tag, Input  } from 'antd'
 import BraftEditor from 'braft-editor'
 import 'braft-editor/dist/braft.css'
 const initEditContent = '<p style="font-size: 14px">这是第一次</p>'
@@ -9,34 +9,71 @@ let that
 export default class DrawContent extends React.Component {
   state = {
     isCheck: true,
-    editContent: initEditContent
+    editContent: initEditContent,
+    isInEdit: false,
+    tagArray: [122,111,555,888],
+    isInAddTag: false
   }
+
+  //有关于富文本编辑---------------start
+  editWrapClick(e) {
+    e.stopPropagation();
+  }
+  goEdit(e) {
+    e.stopPropagation();
+    this.setState({
+      isInEdit: true
+    })
+  }
+  drawerContentOutClick(e) {
+    this.setState({
+      isInEdit: false
+    })
+  }
+  //有关于富文本编辑---------------end
+
+  //标签-------------start
+  randomColorArray() {
+    const colorArr = ['magenta','red','volcano','orange','gold','lime','green','cyan','blue','geekblue','purple']
+    const n =  Math.floor(Math.random() * colorArr.length + 1)-1;
+    return colorArr[n]
+  }
+  tagClose(e) {
+  }
+  addTag() {
+    this.setState({
+      isInAddTag: true
+    })
+  }
+  tagAddComplete(e) {
+    this.setState({
+      isInAddTag: false
+    })
+  }
+  //标签-------------end
+
   render() {
     that = this
-    const { isCheck, editContent } = this.state
+    const { isCheck, editContent, isInEdit, tagArray, isInAddTag } = this.state
     const editorProps = {
       height: 0,
       contentFormat: 'html',
       initialContent: editContent,
       onHTMLChange:(e) => {
-        console.log(e)
         this.setState({
           editContent: e
         })
       },
-      onBlur: () => {
-        console.log('ss')
-      },
       fontSizes: [14],
       controls: [
         'text-color', 'bold', 'italic', 'underline', 'strike-through',
-        'text-align', 'headings', 'list_ul',
+        'text-align', 'list_ul',
         'list_ol', 'blockquote', 'code', 'split', 'media'
       ]
     }
 
     return(
-      <div className={DrawerContentStyles.DrawerContentOut}>
+      <div className={DrawerContentStyles.DrawerContentOut} onClick={this.drawerContentOutClick.bind(this)}>
 
         <div className={DrawerContentStyles.divContent_1}>
           <div className={DrawerContentStyles.contain_1}>
@@ -64,14 +101,52 @@ export default class DrawContent extends React.Component {
           </div>
         </div>
 
+        {/*富文本*/}
+        {!isInEdit ? (
+          <div className={DrawerContentStyles.divContent_1} >
+            <div className={DrawerContentStyles.contain_4} onClick={this.goEdit.bind(this)}>
+              <div dangerouslySetInnerHTML={{__html: editContent}}></div>
+            </div>
+          </div>
+        ) : (
+          <div className={DrawerContentStyles.editorWraper} onClick={this.editWrapClick.bind(this)}>
+            <BraftEditor {...editorProps} style={{fontSize:12}}/>
+          </div>
+        ) }
+
+        {/*关联*/}
         <div className={DrawerContentStyles.divContent_1}>
-          <div className={DrawerContentStyles.contain_4}>
-            <div dangerouslySetInnerHTML={{__html: editContent}}></div>
+          <div className={DrawerContentStyles.contain_6}>
+            <div className={DrawerContentStyles.contain_6_add} onClick={this.addTag.bind(this)}>
+              <Icon type="plus" style={{marginRight: 4}}/>关联内容
+            </div>
           </div>
         </div>
-        <div className={DrawerContentStyles.editorWraper}>
-          <BraftEditor {...editorProps} style={{fontSize:12}}/>
+
+        {/*标签*/}
+        <div className={DrawerContentStyles.divContent_1}>
+          <div className={DrawerContentStyles.contain_5}>
+            {tagArray.map((value, key) => {
+              return(
+                <Tag closable onClose={this.tagClose.bind(this)} key={key} color={this.randomColorArray()}>{value}</Tag>
+              )
+            })}
+            <div>
+              {!isInAddTag ? (
+                <div className={DrawerContentStyles.contain_5_add} onClick={this.addTag.bind(this)}>
+                  <Icon type="plus" style={{marginRight: 4}}/>标签
+                </div>
+              ) : (
+                <Input placeholder={'标签'} style={{height: 22, fontSize: 14, color: '#8c8c8c',minWidth: 62, maxWidth: 100}} onPressEnter={this.tagAddComplete.bind(this)} onBlur={this.tagAddComplete.bind(this)}/>
+              ) }
+            </div>
+
+          </div>
         </div>
+
+        <div className={DrawerContentStyles.spaceLine}></div>
+
+
       </div>
     )
   }
