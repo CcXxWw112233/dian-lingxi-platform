e9d06d6494c429a67c27e7d6be2d6e9a744fe49c
chenxuwei <1109567482@qq.com>
2018-09-11 21:08:37 +0800

    header hover查看个人信息样式问题修复

--- src/routes/Technological/components/HeaderNav/index.js
+++ src/routes/Technological/components/HeaderNav/index.js
@@ -124,7 +124,7 @@ export default class HeaderNav extends React.Component{
               <span className={indexStyle.specificalItem}><Icon type="schedule" /><span className={indexStyle.specificalItemText}>账户设置</span></span>
             </div>
           </Menu.Item>
-          <Menu.Divider key="none_2"  style={{height: 64,padding:0,margin: 0}}/>
+          <Menu.Divider key="none_2"  style={{height: 0,padding:0,margin: 0}}/>
           <Menu.Item key="7" style={{height: 64,padding:0,margin: 0}}>
             <div className={indexStyle.itemDiv_2}>
               <div className={indexStyle.avatar}>
