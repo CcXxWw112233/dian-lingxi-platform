222762e66a70ebab7f85a9c3043c0d83c7a1127c
chenxuwei <1109567482@qq.com>
2018-11-09 11:46:37 +0800

    团队发布列表

--- src/routes/Technological/components/TeamShow/TeamList.js
+++ src/routes/Technological/components/TeamShow/TeamList.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import indexStyles from './index.less'
 import { Icon } from 'antd'
+import TeamListTypeOne from './TeamListTypeOne'
+import TeamListTypeTwo from './TeamListTypeTwo'
 
 export default class TeamList extends React.Component{
+  state={
+    showType: '2', //表现形式1 ， 2
+
+  }
+  setShowType(showType) {
+    this.setState({
+      showType
+    })
+  }
   render(){
-    let templateHtml = ''
-    const minHeight = document.body.clientHeight
+    const { showType } = this.state
     const data = [1,2,3,4]
     return(
       <div className={indexStyles.teamListOut}>
@@ -20,16 +30,17 @@ export default class TeamList extends React.Component{
           </div>
 
           <div className={indexStyles.head_right}>
-            <Icon type="appstore-o" style={{fontSize:16,marginRight: 16}}/><Icon type="hdd" style={{fontSize:16}}/>
+            <Icon onClick={this.setShowType.bind(this, '1')} type="appstore-o" style={{fontSize:16,marginRight: 16, color: showType === '1' ? '#262626':'#8c8c8c'}}/><Icon type="bars" onClick={this.setShowType.bind(this, '2')} style={{fontSize:18, color: showType === '2' ? '#262626':'#8c8c8c'}} />
           </div>
         </div>
-        <div className={indexStyles.teamList_list_1}>
-          {data.map(value => (
-            <div className={indexStyles.teamList_list_1_item}>
-
-            </div>
+        <div className={showType === '1' ? indexStyles.teamList_list_1 : indexStyles.teamList_list_2}>
+          {data.map((value, key ) => (
+            showType === '1' ? (
+              <TeamListTypeOne itemValue={value} itemKey={key} key={key} />
+            ): (
+              <TeamListTypeTwo itemValue={value} itemKey={key} key={key} />
+            )
           ))}
-
         </div>
       </div>
     )
