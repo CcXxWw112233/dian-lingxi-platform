286b2a433e54e65dbb2fd778b96e8194904107a6
chenxuwei <1109567482@qq.com>
2018-12-06 17:35:15 +0800

    部分兼容ie

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import '@babel/polyfill'
+import 'raf/polyfill';
 // import 'react-dom'
 import dva from 'dva';
 import './index.css';
 
 //兼容ie10及以下
 Object.setPrototypeOf = require('setprototypeof');
-
+// var browser=navigator.appName
+// var b_version=navigator.appVersion
+// var version=b_version.split(";");
+// var trim_Version=version[1]
+// if(browser=="Microsoft Internet Explorer")
+// {
+//   alert(trim_Version);
+// }
 // 1. Initialize
 const app = dva({
   // history: createHistory(), //参考自https://www.jianshu.com/p/2e9e45e9a880
