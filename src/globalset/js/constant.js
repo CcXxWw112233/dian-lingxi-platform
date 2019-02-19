21089549ddf440fef389b47badc03fcf5d5a6d84
chenxuwei <1109567482@qq.com>
2019-02-19 11:12:59 +0800

    'changeRequestAPI'

--- src/globalset/js/constant.js
+++ src/globalset/js/constant.js
@@ -1,10 +1,12 @@
-
+const PROJECTS_API = '/api/projects'
 export const REQUEST_DOMAIN = '/api/upms' //接口域名
-export const REQUEST_DOMAIN_BOARD = '/api/team' //接口域名
-export const REQUEST_DOMAIN_FILE = '/api/files' //接口域名
-export const REQUEST_DOMAIN_FLOWS = '/api/flows' //接口域名
-export const REQUEST_DOMAIN_WORK_BENCH = '/api/workbench'
-export const REQUEST_DOMAIN_TEAM_SHOW = '/api/teamshow'
+export const REQUEST_DOMAIN_BOARD = PROJECTS_API //接口域名
+export const REQUEST_DOMAIN_FILE = PROJECTS_API //接口域名
+export const REQUEST_DOMAIN_FLOWS = PROJECTS_API //接口域名
+export const REQUEST_DOMAIN_ABOUT_PROJECT = PROJECTS_API //项目相关接口域名前缀
+
+export const REQUEST_DOMAIN_WORK_BENCH = '/api/workbenchs'
+export const REQUEST_DOMAIN_TEAM_SHOW = '/api/more'
 
 export const REQUEST_DOMAIN_ARTICLE = 'https://knowapi.new-di.com' //微信小程序后台文章列表
 export const WE_APP_TYPE_KNOW_CITY = '1' //知城社
