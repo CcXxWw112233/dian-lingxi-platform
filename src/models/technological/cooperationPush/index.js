8a81196661bf22c99d7ab8fb9483b18b7bbed238
chenxuwei <1109567482@qq.com>
2019-03-04 17:39:47 +0800

    工作台协作联调

--- src/models/technological/cooperationPush/index.js
+++ src/models/technological/cooperationPush/index.js
@@ -24,7 +24,22 @@ import {
   selectFilePreviewCommitPoints,
   selectProjectDetailInfoData,
 } from './../select'
-
+import {
+  workbench_selectProjectList,
+  workbench_selectDrawContent,
+  workbench_selectCard_id,
+  workbench_selectrResponsibleTaskList,
+  workbench_selectrMeetingLsit,
+  workbench_selectrBackLogProcessList,
+  workbench_selectrUploadedFileList,
+  workbench_selectFilePreviewCurrentFileId,
+  workbench_selectBreadcrumbList,
+  workbench_selectFileList,
+  workbench_selectFilePreviewCommits,
+  workbench_selectFilePreviewPointNumCommits,
+  workbench_selectFilePreviewCommitPoints,
+  workbench_selectFilePreviewCommitPointNumber
+} from '../workbench/selects'
 //定义model名称
 const model_projectDetail = name => `projectDetail/${name}`
 const model_projectDetailTask = name => `projectDetailTask/${name}`
@@ -38,7 +53,8 @@ const model_workbenchFileDetail = name => `workbenchFileDetail/${name}`
 const model_workbenchPublicDatas = name => `workbenchPublicDatas/${name}`
 
 //消息推送model
-let dispathes
+let dispathes = null
+let locationPath = null
 export default {
   namespace: 'cooperationPush',
   state: [],
@@ -46,6 +62,7 @@ export default {
     setup({ dispatch, history }) {
       dispathes = dispatch
       history.listen((location) => {
+        locationPath = location.pathname
         message.destroy()
         //头部table key
         if (location.pathname.indexOf('/technological') !== -1) {
@@ -95,15 +112,23 @@ export default {
         return;
       }
       data = JSON.parse(data)
+      let handleType = 'handleWsData_board_detail'
+      if(locationPath.indexOf('technological/workbench') != -1) {
+        handleType = 'handleWsData_workbench'
+      } else if(locationPath.indexOf('technological/projectDetail') != -1) {
+        handleType = 'handleWsData_board_detail'
+      } else {
+
+      }
       yield put({
-        type: 'handleWsData',
+        type: handleType,
         payload: {
           res: data
         }
       })
     },
 
-    * handleWsData({ payload }, { call, put, select }) {
+    * handleWsData_board_detail({ payload }, { call, put, select }) {
       const { res } = payload
       const { data } = res
       console.log('eee', data)
@@ -515,6 +540,348 @@ export default {
       }
 
     },
+
+    * handleWsData_workbench({ payload }, { call, put, select }) {
+      const { res } = payload
+      const { data } = res
+      console.log('eee', data)
+      const currentProjectBoardId = yield select(selectProjectDetailBoardId)
+
+      let coperate = data[0] //协作
+      let news = data[1] //消息
+      //获取消息协作类型
+      const coperateName = coperate.e
+      const coperateType = coperateName.substring(0, coperateName.indexOf('/'))
+      let coperateData = JSON.parse(coperate.d)
+      console.log('eee', coperateData)
+
+      const getAfterNameId = (coperateName) => { //获取跟在名字后面的id
+        return coperateName.substring(coperateName.indexOf('/') + 1)
+      }
+
+      let board_id_
+      switch (coperateType) {
+        case 'change:board':
+          let op_board_id = getAfterNameId(coperateName)
+          let is_deleted_ = coperateData['is_deleted']
+          let projectList = yield select(workbench_selectProjectList) || []
+          let arr = [...projectList]
+          if(is_deleted_ == '0') {
+            for(let i = 0; i < projectList.length;i++) {
+              if(op_board_id == projectList[i]['board_id']) {
+                arr.splice(i, 1, coperateData)
+                break
+              }
+            }
+          } else if(is_deleted_ == '1'){
+            for(let i = 0; i < projectList.length;i++) {
+              if(op_board_id == projectList[i]['board_id']) {
+                arr.splice(i, 1)
+                break
+              }
+            }
+          }
+          dispathes({
+            type: model_workbench('updateDatas'),
+            payload: {
+              projectList: arr
+            }
+          })
+          break
+        case 'change:card': //监听到修改任务
+          const drawContent = yield select(workbench_selectDrawContent)
+          const card_id = yield select(workbench_selectCard_id)
+          const operate_card_id = coperateName.substring(coperateName.indexOf('/') + 1)
+          const task_list = yield select(workbench_selectrResponsibleTaskList) || []
+          let is_archived_ = coperateData['is_archived']
+
+          if(is_archived_ == '1') { //归档
+            for(let i = 0; i < task_list.length; i++) {
+              if(operate_card_id == task_list[i]['id']) {
+                task_list.splice(i, 1)
+                break
+              }
+            }
+          }
+          //如果当前查看的任务和推送的任务id一样，会发生更新
+          if(card_id == operate_card_id) {
+            if(is_archived_ == '1') {
+              dispathes({
+                type: model_workbenchTaskDetail('updateDatas'),
+                payload: {
+                  drawerVisible: false
+                }
+              })
+            }
+
+            dispathes({
+              type: model_workbenchTaskDetail('updateDatas'),
+              payload: {
+                drawContent: {...drawContent, ...coperateData}
+              }
+            })
+          }
+          dispathes({
+            type: model_projectDetailTask('updateDatas'),
+            payload: {
+              responsibleTaskList: task_list
+            }
+          })
+          break
+        case 'change:cards':
+          let task_list_ = yield select(workbench_selectrResponsibleTaskList) || []
+          let meetingList = yield select(workbench_selectrMeetingLsit) || []
+          let id_arr_ = getAfterNameId(coperateName).split('/')
+          let board_id_ = id_arr_[0]
+          let list_id = id_arr_[1]
+          let { is_archived, is_deleted } = coperateData
+          let card_type = coperateData['type']
+          let is_has_realize = false //插入还是push标志
+          let cObj = {...coperateData, name: coperateData['card_name'], id: coperateData['card_id'], board_id: board_id_}
+
+          if(card_type == '0') { //任务
+            for(let i = 0; i < task_list_.length; i++ ) {
+              //如果某一列里面有完成的任务，则在完成的任务前面增加一条，否则直接往后塞
+              if(task_list_[i]['is_realize'] == '1') {
+                is_has_realize = true
+                task_list_.splice(i, 0, cObj)
+                break
+              }
+
+            }
+          }else if(card_type == '1') { //会议
+            meetingList.push(cObj)
+          } else {
+
+          }
+          if(!is_has_realize) {
+            if(card_type == '0') {
+              task_list_.push(cObj)
+            }
+          }
+          dispathes({
+            type: model_workbenchTaskDetail('updateDatas'),
+            payload: {
+              responsibleTaskList: task_list_,
+              meetingLsit: meetingList
+            }
+          })
+          break
+        case 'delete:cards':
+          id_arr_ = getAfterNameId(coperateName).split('/')
+          task_list_ = yield select(workbench_selectrResponsibleTaskList)
+          meetingList = yield select(workbench_selectrMeetingLsit) || []
+          board_id_ = id_arr_[0]
+          list_id = id_arr_[1]
+          let op_card_id = coperateData['card_id']
+          for(let i = 0; i < task_list_.length; i++) {
+            if(op_card_id == task_list_[i]['id']) {
+              task_list_.splice(i, 1)
+              break
+            }
+          }
+          for(let i = 0; i < meetingList.length; i++) {
+            if(op_card_id == meetingList[i]['id']) {
+              meetingList.splice(i, 1)
+              break
+            }
+          }
+
+          dispathes({
+            type: model_workbenchTaskDetail('updateDatas'),
+            payload: {
+              responsibleTaskList: task_list_,
+              meetingLsit: meetingList
+            }
+          })
+          break
+        case 'change:flow:instance':
+          // board_id_
+          // if(board_id_ == currentProjectBoardId) {
+          //   const processList = yield select(selectCurrentProcessList)
+          //   processList.push(coperateData)
+          //   dispathes({
+          //     type: model_projectDetailProcess('updateDatas'),
+          //     payload: {
+          //       processList
+          //     }
+          //   })
+          // }
+          break
+        case 'change:flow':
+          // const currentProcessInstanceId = yield select(selectCurrentProcessInstanceId)
+          // const flow_id = getAfterNameId(coperateName)
+          // if(currentProcessInstanceId == flow_id) {
+          //   const curr_node_id = coperateData.curr_node_id
+          //   let curr_node_sort
+          //   for (let i = 0; i < coperateData.nodes.length; i++ ) {
+          //     if(curr_node_id === coperateData.nodes[i].id) {
+          //       curr_node_sort = coperateData.nodes[i].sort
+          //       break
+          //     }
+          //   }
+          //   curr_node_sort = curr_node_sort || coperateData.nodes.length + 1 //如果已全部完成了会是一个undefind,所以给定一个值
+          //   dispathes({
+          //     type: model_projectDetailProcess('updateDatas'),
+          //     payload: {
+          //       processInfo: {...coperateData, curr_node_sort},
+          //       processEditDatas: coperateData.nodes || [],
+          //     }
+          //   })
+          // }
+
+          break
+        case 'change:file':
+          //增减filelist
+          //当当前的文件夹id 和操作的文件id的所属文件夹id 一样
+          const uploadedFileList = yield select(workbench_selectrUploadedFileList)
+          const fileType = coperateData.type
+          const { status } = coperateData
+          //status 2删除
+          if(status == '2') {
+            const { id } = coperateData
+            for( let i =0; i < uploadedFileList.length; i++ ) {
+              if(uploadedFileList[i]['id'] == id) {
+                uploadedFileList.splice(i, 1)
+                break
+              }
+            }
+            dispathes({
+              type: model_workbench('updateDatas'),
+              payload: {
+                uploadedFileList
+              }
+            })
+          }
+          break
+        case 'change:file:comment':
+          const comment_file_id = getAfterNameId(coperateName)
+          let file_id = yield select(workbench_selectFilePreviewCurrentFileId)
+          if(comment_file_id == file_id) { //如果推送评论的文档id和查看的id是一样
+            const filePreviewCommits = yield select(workbench_selectFilePreviewCommits) || []
+            const filePreviewPointNumCommits = yield select(workbench_selectFilePreviewPointNumCommits) || []
+            const filePreviewCommitPointNumber = yield select(workbench_selectFilePreviewCommitPointNumber) || []
+            const filePreviewCommitPoints = yield select(workbench_selectFilePreviewCommitPoints) || []
+
+            let pointNo = coperateData['flag']
+            if(pointNo) { //圈评
+              if(pointNo == filePreviewCommitPointNumber) { //如果是当前圈评的这个点
+                filePreviewPointNumCommits.push(coperateData)
+              } else {
+                let isHasPoint = false
+                //如果没有这个点，则添加这个点
+                for(let i = 0; i < filePreviewCommitPoints.length; i++ ) {
+                  if(filePreviewCommitPoints[i]['flag'] == pointNo) {
+                    isHasPoint = true
+                    break
+                  }
+                }
+                if(!isHasPoint) {
+                  filePreviewCommitPoints.push(coperateData)
+                }
+              }
+            }
+            filePreviewCommits.push(coperateData)
+            dispathes({
+              type: model_workbenchFileDetail('updateDatas'),
+              payload: {
+                filePreviewCommits,
+                filePreviewPointNumCommits,
+                filePreviewCommitPoints
+              }
+            })
+          }
+          break
+        case 'change:file:comment:delete':
+          let commitId = getAfterNameId(coperateName)
+          const filePreviewCommits = yield select(workbench_selectFilePreviewCommits) || []
+          const filePreviewPointNumCommits = yield select(workbench_selectFilePreviewPointNumCommits) || []
+          const filePreviewCommitPointNumber = yield select(workbench_selectFilePreviewCommitPointNumber) || []
+          const filePreviewCommitPoints = yield select(workbench_selectFilePreviewCommitPoints) || []
+
+          //删除点 存在最后一条is_last和点flag
+          let flag_ = coperateData['flag']
+          let is_last = coperateData['is_last']
+          if(flag_ && is_last == '1') {
+            for(let i = 0; i < filePreviewCommitPoints.length; i ++) {
+              if(filePreviewCommitPoints[i]['flag'] == flag_) {
+                filePreviewCommitPoints.splice(i, 1)
+                break
+              }
+            }
+          }
+
+          if(filePreviewPointNumCommits){ //处理点的评论
+            for(let i = 0; i < filePreviewPointNumCommits.length; i ++) {
+              if(filePreviewPointNumCommits[i]['id'] == commitId) {
+                filePreviewPointNumCommits.splice(i, 1)
+                break
+              }
+            }
+          }
+
+          if(filePreviewCommits) { //处理整体评论
+            for(let i = 0; i < filePreviewCommits.length; i ++) {
+              if(filePreviewCommits[i]['id'] == commitId) {
+                filePreviewCommits.splice(i, 1)
+                break
+              }
+            }
+          }
+
+          dispathes({
+            type: model_workbenchFileDetail('updateDatas'),
+            payload: {
+              filePreviewCommits,
+              filePreviewPointNumCommits,
+              filePreviewCommitPoints
+            }
+          })
+          break
+        case 'change:file:operation': //移动和复制
+          // let ids = getAfterNameId(coperateName)
+          // let idArr = ids.split('/')
+          // let fileList_ = yield select(selectFileList)
+          // let folder_id = yield select(selectCurrentParrentDirectoryId)
+          // let coFileList = coperateData['file_list']
+          // if(idArr.length == 1) { //复制
+          //   if(idArr[0] == folder_id) {
+          //     fileList_ = [].concat(fileList_, coFileList)
+          //   }
+          // }else if(idArr.length == 2) { //移动
+          //   if(idArr[0] != idArr[1]) { ////移入的文件和移除的文件夹不是同一个
+          //     if(idArr[1] == folder_id) { //当前文件夹有文件移除
+          //       for(let i = 0; i< fileList_.length; i++) {
+          //         for (let j = 0; j < coFileList.length; j++) {
+          //           if(fileList_[i]['file_id'] == coFileList[j]['file_id']) {
+          //             fileList_.splice(i, 1)
+          //             break
+          //           }
+          //
+          //         }
+          //       }
+          //     }else if(idArr[0] == folder_id){ //当前文件夹有文件移进
+          //       fileList_ = [].concat(fileList_, coFileList)
+          //     }
+          //
+          //   }
+          // } else {
+          //
+          // }
+          // dispathes({
+          //   type: model_projectDetailFile('updateDatas'),
+          //   payload: {
+          //     fileList: fileList_
+          //   }
+          // })
+
+          break
+        default:
+          break
+      }
+
+    },
+
 
     * routingJump({ payload }, { call, put }) {
       const { route } = payload
