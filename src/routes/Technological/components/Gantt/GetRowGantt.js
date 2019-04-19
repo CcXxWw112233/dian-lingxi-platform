c3bcb63ae71ab8928fad4ddba653a2cd96b01924
1109567482@qq.com <cxw2385>
2019-04-19 18:08:48 +0800

    '优化move算法'

--- src/routes/Technological/components/Gantt/GetRowGantt.js
+++ src/routes/Technological/components/Gantt/GetRowGantt.js
@@ -15,89 +15,60 @@ export default class GetRowGantt extends Component {
     }
     this.x1 = 0 //用于做拖拽生成一条任务
     this.y1 = 0
-    this.isDragging = false
-    this.isMouseDown = false
+    this.isDragging = false //判断是否在拖拽虚线框
+    this.isMouseDown = false //是否鼠标按下
     this.SelectedRect = {x: 0, y: 0 }
 
-    this.x2 = 0 //用于鼠标hover显示一个虚线框
-    this.y2 = 0
   }
 
-  isObj(obj) {
-    if(!obj || typeof obj !=='object') {
-      return false
-    } else {
-      return true
-    }
-  }
   stopDragging() {
     const target = this.refs.operateArea
     target.onmousemove = null;
     target.onmuseup = null;
+    const that = this
+    setTimeout(function () {
+      that.isDragging = false
+      that.isMouseDown = false
+    }, 1000)
   }
   dashedMousedown(e) {
     const { currentRect = {} } = this.state
-    // 取得target上被单击的点
-    const target = this.refs.operateArea//event.target || event.srcElement;
     this.x1 = currentRect.x
     this.y1 = currentRect.y
-    this.SelectedRect = {x: 0, y: 0 }
     this.isDragging = false
     this.isMouseDown = true
-    /*定义鼠标移动事件*/
+    const target = this.refs.operateArea//event.target || event.srcElement;
     target.onmousemove = this.dashedDragMousemove.bind(this);
-    /*定义鼠标抬起事件*/
     target.onmouseup = this.dashedDragMouseup.bind(this);
   }
   dashedDragMousemove(e) {
-    //mousedown 后开始拖拽时添加
+    this.isDragging = true
     const { datas: { ceiHeight, ceilWidth }} = this.props.model
 
-    if(!this.isDragging) {
-      const property = {
-        x: this.x1,
-        y: this.y1,
-        width: 44,
-        height: 20,
-      }
-      this.setState({
-        currentRect: property,
-      })
-
-    }
-
-    // 判断矩形是否开始拖拽
     const target_0 = document.getElementById('gantt_card_out')
     const target_1 = document.getElementById('gantt_card_out_middle')
     const target = this.refs.operateArea//event.target || event.srcElement;
-    this.isDragging = true
 
-    // 判断拖拽对象是否存在
-    if (this.isObj(this.SelectedRect)) {
-      // 取得鼠标位置
-      const x = e.pageX - target_0.offsetLeft + target_1.scrollLeft - 20
-      const y = e.pageY - target.offsetTop - 60
+    // 取得鼠标位置
+    const x = e.pageX - target_0.offsetLeft + target_1.scrollLeft - 20
+    const y = e.pageY - target.offsetTop - 60
+    //设置宽度
+    const offset_left = Math.abs(x - this.x1);
+    // 更新拖拽的最新矩形
+    let px = x < this.x1 ? x : this.x1 //向左向右延申
+    let py = this.y1
+    let width = offset_left < ceilWidth ? ceilWidth : offset_left
 
-      //设置高度
-      this.SelectedRect.x = x - this.x1;
-      this.SelectedRect.y = y - this.y1;
-
-      // 更新拖拽的最新矩形
-      let px = x < this.x1 ? this.x1 - Math.abs(this.SelectedRect.x) : x - Math.abs(this.SelectedRect.x)
-      let py = this.y1
-      let width = Math.abs(this.SelectedRect.x) < ceilWidth ? ceilWidth : Math.abs(this.SelectedRect.x)
-
-      const property ={
-        x: px,
-        y: py,
-        width,
-        height: 20,
-      }
-
-      this.setState({
-        currentRect: property
-      })
+    const property ={
+      x: px,
+      y: py,
+      width,
+      height: 20,
     }
+
+    this.setState({
+      currentRect: property
+    })
   }
   dashedDragMouseup() {
     this.stopDragging()
@@ -113,47 +84,33 @@ export default class GetRowGantt extends Component {
         dasheRectShow: true
       })
     }
+
     const target_0 = document.getElementById('gantt_card_out')
     const target_1 = document.getElementById('gantt_card_out_middle')
-    this.x2 = e.clientX - target_0.offsetLeft + target_1.scrollLeft - 20
-    this.y2 = e.clientY - target_0.offsetTop - 60
+    // 取得鼠标位置
+    let px = e.pageX - target_0.offsetLeft + target_1.scrollLeft - 20
+    let py = e.pageY - target_0.offsetTop - 60
 
-    // 判断拖拽对象是否存在
-    if (this.isObj(this.SelectedRect)) {
-      // 取得鼠标位置
-      const x = e.pageX - target_0.offsetLeft + target_1.scrollLeft - 20
-      const y = e.pageY - target_0.offsetTop - 60
+    const molX = px % ceilWidth
+    const molY = py % ceiHeight
+    const mulX = Math.floor(px / ceilWidth)
+    const mulY = Math.floor(py / ceiHeight)
+    const delX = Number((molX / ceilWidth).toFixed(1))
+    const delY = Number((molY / ceiHeight).toFixed(1))
 
-      //------------------------
-      //设置left 和 top
-      this.SelectedRect.x = x - this.x2;
-      this.SelectedRect.y = y - this.y2;
+    px = px - molX
+    py = py - molY
 
-      // 更新拖拽的最新矩形
-      let px = x < this.x2 ? this.x2 - Math.abs(this.SelectedRect.x) : x - Math.abs(this.SelectedRect.x)
-      let py = y < this.y1 ? this.y1 - Math.abs(this.SelectedRect.y) : y - Math.abs(this.SelectedRect.y)
-
-      const molX = px % ceilWidth
-      const molY = py % ceiHeight
-      const mulX = Math.floor(px / ceilWidth)
-      const mulY = Math.floor(py / ceiHeight)
-      const delX = Number((molX / ceilWidth).toFixed(1))
-      const delY = Number((molY / ceiHeight).toFixed(1))
-
-      px = px - molX
-      py = py - molY
-
-      const property ={
-        x: px,
-        y: py,
-        width: 40,
-        height: 20,
-      }
-
-      this.setState({
-        currentRect: property
-      })
+    const property ={
+      x: px,
+      y: py,
+      width: 40,
+      height: 20,
     }
+
+    this.setState({
+      currentRect: property
+    })
   }
   dashedMouseLeave(e) {
     if(!this.isMouseDown) {
@@ -196,69 +153,4 @@ export default class GetRowGantt extends Component {
 function mapStateToProps({ modal, gantt, loading }) {
   return { modal, model: gantt, loading }
 }
-// dashedMousedown(e) {
-//   // 取得target上被单击的点
-//   const target_0 = document.getElementById('gantt_card_out')
-//   const target_1 = document.getElementById('gantt_card_out_middle')
-//   const target = this.refs.operateArea//event.target || event.srcElement;
-//   this.x1 = e.clientX - target_0.offsetLeft + target_1.scrollLeft - 20
-//   this.y1 = e.pageY - target.offsetTop - 60
-//   this.SelectedRect = {x: 0, y: 0 }
-//   this.isDragging = false
-//   this.isMouseDown = true
-//   /*定义鼠标移动事件*/
-//   target.onmousemove = this.dashedDragMousemove.bind(this);
-//   /*定义鼠标抬起事件*/
-//   target.onmouseup = this.dashedDragMouseup.bind(this);
-// }
-// dashedDragMousemove(e) {
-//   //mousedown 后开始拖拽时添加
-//   if(!this.isDragging) {
-//     const property = {
-//       x: this.x1,
-//       y: this.y1,
-//       width: this.SelectedRect.x,
-//       height: 20,
-//     }
-//     this.setState({
-//       currentRect: property,
-//     })
-//
-//   }
-//
-//   // 判断矩形是否开始拖拽
-//   const target_0 = document.getElementById('gantt_card_out')
-//   const target_1 = document.getElementById('gantt_card_out_middle')
-//   const target = this.refs.operateArea//event.target || event.srcElement;
-//   this.isDragging = true
-//
-//   // 判断拖拽对象是否存在
-//   if (this.isObj(this.SelectedRect)) {
-//     // 取得鼠标位置
-//     const x = e.pageX - target_0.offsetLeft + target_1.scrollLeft - 20
-//     const y = e.pageY - target.offsetTop - 60
-//
-//     //------------------------
-//     //设置高度
-//     this.SelectedRect.x= x-this.x1;
-//     this.SelectedRect.y= y-this.y1;
-//
-//
-//     // 更新拖拽的最新矩形
-//     let px = x < this.x1 ? this.x1 - Math.abs(this.SelectedRect.x) : x - Math.abs(this.SelectedRect.x)
-//     let py = this.y1//y < this.y1 ? this.y1 - Math.abs(this.SelectedRect.y) : y - Math.abs(this.SelectedRect.y)
-//     let width = Math.abs(this.SelectedRect.x)
-//
-//     const property ={
-//       x: px,
-//       y: py,
-//       width,
-//       height: 20,
-//     }
-//
-//     // console.log(property)
-//     this.setState({
-//       currentRect: property
-//     })
-//   }
-// }
+
