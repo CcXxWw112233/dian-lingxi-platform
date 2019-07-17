3f239835383a663b6525e2b0df9a698f20ce048b
1109567482@qq.com <cxw2385>
2019-07-17 15:04:58 +0800

    请求头加上baseinfo的逻辑处理

--- src/utils/requestAxios.js
+++ src/utils/requestAxios.js
@@ -36,34 +36,37 @@ export default function request(options = {}, elseSet = {}) {
 
   header['Authorization'] = Authorization//refreshToken
   header['refreshToken'] = refreshToken
-  header['OrganizationId'] = localStorage.getItem('OrganizationId') || '0'
-  header['BoardId'] = localStorage.getItem('storageCurrentOperateBoardId') || '0'//当前操作项目的项目id
-
-  if(data['_organization_id'] || params['_organization_id']) {
-    header['OrganizationId'] = data['_organization_id'] || params['_organization_id']
-  }
-
-  // header['baseInfo'] = {
-  //   organizationId: localStorage.getItem('OrganizationId') || '0',
-  //   boardId: localStorage.getItem('storageCurrentOperateBoardId') || '0',
-  //   contentDataType:'card',
-  //   contentDataId  :'7657665646576547654',
-  //   aboutBoardOrganizationId: localStorage.getItem('aboutBoardOrganizationId') || '0' ,
+  
+  // header['OrganizationId'] = localStorage.getItem('OrganizationId') || '0'
+  // header['BoardId'] = localStorage.getItem('storageCurrentOperateBoardId') || '0'//当前操作项目的项目id
+  // if(data['_organization_id'] || params['_organization_id']) {
+  //   header['OrganizationId'] = data['_organization_id'] || params['_organization_id']
   // }
-  // 请求头BaseInfo base64加密
+
+  // 请求头BaseInfo base64加密(后台权限拦截)，最终输出
+  //BaseInfo: { organizationId，boardId, contentDataType, contentDataId,  aboutBoardOrganizationId, }
+  let header_baseInfo_orgid = localStorage.getItem('OrganizationId') || '0'
+  if(data['_organization_id'] || params['_organization_id']) {
+    header_baseInfo_orgid = data['_organization_id'] || params['_organization_id']
+  }
   const header_BaseInfo = Object.assign({
-      organizationId: localStorage.getItem('OrganizationId') || '0',
+      orgId: header_baseInfo_orgid,
       boardId: localStorage.getItem('storageCurrentOperateBoardId') || '0',
       aboutBoardOrganizationId: localStorage.getItem('aboutBoardOrganizationId') || '0' ,
-  }, headers['BaseInfo'])
+  }, headers['BaseInfo'] || {})
   const header_baseInfo_str = JSON.stringify(header_BaseInfo)
-  header['BaseInfo'] = Base64.encode(header_baseInfo_str)
+  // console.log('sssss_1', header_baseInfo_str)
+  const header_baseInfo_str_base64 = Base64.encode(header_baseInfo_str)
+  // console.log('sssss_2', header_baseInfo_str_base64)
+  const header_base_info = {
+    BaseInfo:  header_baseInfo_str_base64
+  }
 
   return new Promise((resolve, reject) => {
     axios({
       ...{
         url,
-        headers: {...header, ...headers},
+        headers: {...header, ...headers, ...header_base_info},
         method,
         params: {
           ...params, 
