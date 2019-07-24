c8ae947dbd039b3e96d4b654cfdfdaed2709c701
1109567482@qq.com <cxw2385>
2019-07-24 17:34:30 +0800

    导致页面一直反复调用接口和渲染问题修=>techonological页面的index组件connect冗余数据

--- src/routes/Technological/index.js
+++ src/routes/Technological/index.js
@@ -2,24 +2,18 @@ import React from 'react';
 import { connect, } from 'dva';
 import QueueAnim from 'rc-queue-anim'
 import globalClassNmae from '../../globalset/css/globalClassName.less'
-import HeaderNav from './components/HeaderNav'
 import { Route, Router, Switch, Link } from 'dva/router'
 import dynamic from "dva/dynamic";
 import dva from "dva/index";
-import indexStyles from './index.less'
-import modelExtend from 'dva-model-extend'
-import ClassBasicModel from '../../models/technological'
 import { LocaleProvider, Icon, Layout, Menu, } from 'antd';
 import zh_CN from 'antd/lib/locale-provider/zh_CN';
 import 'moment/locale/zh-cn';
-import ImChat from './ImChat.js'
 import SiderLeft from './Sider/SiderLeft'
 import SiderRight from './Sider/SiderRight'
 import GlobalSearch from './GlobalSearch'
 
 const { Header, Sider, Content } = Layout;
 
-const getEffectOrReducerByName = name => `technological/${name}`
 @connect(mapStateToProps)
 export default class Technological extends React.Component {
 
@@ -39,85 +33,9 @@ export default class Technological extends React.Component {
 
   render() {
 
-    const { dispatch, model } = this.props;
-    const { datas = {} } = model;
-    const { page_load_type = 0 } = datas;
-
+    const { page_load_type } = this.props;
+    
     const app = dva();
-    //导航栏props-------------
-    const HeaderNavProps = {
-      model,
-      dispatch,
-      getMenuList(data) {
-        // console.log('this is parents')
-        dispatch({
-          type: getEffectOrReducerByName('getMenuList'),
-          data
-        })
-      },
-      logout() {
-        dispatch({
-          type: getEffectOrReducerByName('logout'),
-        })
-      },
-      routingJump(path) {
-        dispatch({
-          type: getEffectOrReducerByName('routingJump'),
-          payload: {
-            route: path,
-          },
-        })
-      },
-      updateDatas(payload) {
-        dispatch({
-          type: getEffectOrReducerByName('updateDatas'),
-          payload: payload
-        })
-      },
-      //组织
-      getSearchOrganizationList(data) {
-        dispatch({
-          type: getEffectOrReducerByName('getSearchOrganizationList'),
-          payload: data
-        })
-      },
-      createOrganization(data) {
-        dispatch({
-          type: getEffectOrReducerByName('createOrganization'),
-          payload: data
-        })
-      },
-      updateOrganization(data) {
-        dispatch({
-          type: getEffectOrReducerByName('updateOrganization'),
-        })
-      },
-      applyJoinOrganization(data) {
-        dispatch({
-          type: getEffectOrReducerByName('applyJoinOrganization'),
-          payload: data
-        })
-      },
-      inviteJoinOrganization(data) {
-        dispatch({
-          type: getEffectOrReducerByName('inviteJoinOrganization'),
-          payload: data
-        })
-      },
-      uploadOrganizationLogo(data) {
-        dispatch({
-          type: getEffectOrReducerByName('uploadOrganizationLogo'),
-          payload: data
-        })
-      },
-      changeCurrentOrg(data) {
-        dispatch({
-          type: getEffectOrReducerByName('changeCurrentOrg'),
-          payload: data
-        })
-      }
-    }
-    //-----------------
 
     const routes = [
       {
@@ -156,7 +74,7 @@ export default class Technological extends React.Component {
     const defaultLayout = (
       <Layout >
         <Sider collapsedWidth={64} theme={'light'} collapsed={true} />
-        <SiderLeft {...HeaderNavProps} />
+        <SiderLeft />
         <Layout style={{ backgroundColor: 'rgba(245,245,245,1)' }}>
           <Content style={{
             margin: '0 16px',
@@ -238,7 +156,12 @@ export default class Technological extends React.Component {
 };
 
 //  建立一个从（外部的）state对象到（UI 组件的）props对象的映射关系
-function mapStateToProps({ modal, technological, loading }) {
-  return { modal, model: technological, loading }
+function mapStateToProps({technological: {
+  datas: {
+    page_load_type
+  }
 }
-// export default connect(mapStateToProps)(Technological)
+}) {
+  return { page_load_type }
+}
+
