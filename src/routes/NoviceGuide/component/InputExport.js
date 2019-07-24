e7f583518c41cb6d435ad0fd05142d9c3c250584
ljf <ljf@qq.com>
2019-07-24 19:00:47 +0800

    '验证的实现一部分'

--- src/routes/NoviceGuide/component/InputExport.js
+++ src/routes/NoviceGuide/component/InputExport.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react'
 import { Input } from 'antd'
 import glabalStyles from '@/globalset/css/globalClassName.less'
 import styles from '../index.less'
+import { validateTel, validateEmail } from '@/utils/verify.js'
+let timer;
 
 export default class InputExport extends Component {
 
+  state = {
+    timer: null,
+    phone_val: '',
+    email_val: '',
+    is_show_tips: 'none', // 是否显示小图标 默认为none不显示
+    is_verify_tips: false, // 验证提示小图标是否成功, 默认为false,验证失败 true 表示成功
+  }
+
   chgVal = (e) => {
-    console.log(e, 'sss')
+    console.log(e.target.value, 'sss')
+    let val = e.target.value
+    // console.log(validateTel(val), 'ssss')
+    if (timer) {
+      clearTimeout(timer)
+    }
+    // this.setState({
+    //   timer: setTimeout(() => {
+    //     let phone_val_verify = validateTel(val)
+    //     console.log(phone_val_verify, 'sss')
+    //   }, 500)
+    // })
+
+
   }
 
   render() {
