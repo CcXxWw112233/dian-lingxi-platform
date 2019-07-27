63af135f337f8c4ad1e2ee5df4d49645e876de4d
ljf <ljf@qq.com>
2019-07-27 14:52:47 +0800

    '工作台动态的维护'

--- src/routes/Technological/components/Workbench/PersonNews/component/InitialNews.js
+++ src/routes/Technological/components/Workbench/PersonNews/component/InitialNews.js
@@ -52,7 +52,7 @@ export default class InitialNews extends React.Component {
 
   // 去到项目详情
   goToBoard({org_id, content}) {
-    console.log(checkIsHasPermission(ORG_TEAM_BOARD_QUERY, org_id), 'sss')
+    // console.log(checkIsHasPermission(ORG_TEAM_BOARD_QUERY, org_id), 'sss')
     if(!checkIsHasPermission(ORG_TEAM_BOARD_QUERY, org_id)){
       message.warn(NOT_HAS_PERMISION_COMFIRN, MESSAGE_DURATION_TIME)
       return false
@@ -91,7 +91,7 @@ export default class InitialNews extends React.Component {
   render() {
 
     const { datas: { newsDynamicList = [], next_id, isHasMore = true, isHasNewDynamic }} = this.props.model
-    console.log(newsDynamicList, 'ssss')
+    // console.log(newsDynamicList, 'ssss')
     const { currentUserOrganizes = [], is_show_org_name } = this.props
     // console.log('this is issues model ---->>>', this.props.model.datas  )
     //过滤消息内容
