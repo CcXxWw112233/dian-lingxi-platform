38e23a202ed4227470e8160b77ed4d87da792a33
1109567482@qq.com <cxw2385>
2019-09-25 15:55:51 +0800

    bug

--- src/routes/SimpleMode/index.js
+++ src/routes/SimpleMode/index.js
@@ -18,13 +18,13 @@ class SimpleMode extends Component {
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll.bind(this)) //监听滚动
-    window.addEventListener('resize', this.handleResize.bind(this)) //监听窗口大小改变
+    window.addEventListener('scroll', this.handleScroll, false) //监听滚动
+    window.addEventListener('resize', this.handleResize, false) //监听窗口大小改变
   }
 
   componentWillUnmount() { //一定要最后移除监听器，以防多个组件之间导致this的指向紊乱
-    window.removeEventListener('scroll', this.handleScroll.bind(this))
-    window.removeEventListener('resize', this.handleResize.bind(this))
+    window.removeEventListener('scroll', this.handleScroll, false)
+    window.removeEventListener('resize', this.handleResize, false)
   }
 
   handleScroll = e => {
@@ -39,7 +39,7 @@ class SimpleMode extends Component {
 
   handleResize = e => {
     const { dispatch, chatImVisiable } = this.props;
-    console.log('浏览器窗口大小改变事件', e.target.innerWidth);
+    // console.log('浏览器窗口大小改变事件', e.target.innerWidth);
     const width = document.body.scrollWidth;
     let rightWidth = 0;
     if (chatImVisiable) {
