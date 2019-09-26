e795ce6534b72aeae356847ded890c1e8e4092d1
1109567482@qq.com <cxw2385>
2019-09-26 18:48:24 +0800

    流程权限的控制

--- src/routes/Technological/components/ProjectDetail/Process/ProcessDetail/DetailConfirmInfoOne.js
+++ src/routes/Technological/components/ProjectDetail/Process/ProcessDetail/DetailConfirmInfoOne.js
@@ -146,7 +146,7 @@ export default class DetailConfirmInfoOne extends React.Component {
     const { datas: { processEditDatas, projectDetailInfoData = [], processInfo = {}, relations_Prefix } } = this.props.model
     const { itemKey, itemValue } = this.props //所属列表位置
     const { board_id } = projectDetailInfoData
-    const { curr_node_sort, status } = processInfo //当前节点
+    const { curr_node_sort, status } = processInfo //当前节点 status 1/2/3 运行中/终止/结束
     const { id, name, description, assignees = [], assignee_type, deadline_type, deadline, deadline_value, is_workday, sort, enable_opinion, enable_revocation } = processEditDatas[itemKey]
     // console.log( processEditDatas[itemKey])
     //推进人来源
@@ -155,10 +155,21 @@ export default class DetailConfirmInfoOne extends React.Component {
     const assigneesArray = assignees || []
     //判断当前用户是否有操作权限--从推进人列表里面获得id，和当前操作人的id
     let currentUserCanOperate = false
+    let currentUserCanReback = true //用户是否有撤回权限，属于操作权限的子范畴
     const userInfo = JSON.parse(localStorage.getItem('userInfo'))
     const currentUserId = userInfo.id //当前用户id, 用于替换
-    for (let i = 0; i < assignees.length; i++) {
-      if (assignees[i].user_id === currentUserId) {
+    //任何人的情况下,需要匹配在不在项目内，否则需要确认是否是指定的推进人
+    let compare_user_arr = []
+    if (assignee_type == '1') {
+      compare_user_arr = users
+      if(typeof assignees == 'Array') {
+        currentUserCanReback = assignees.findIndex(item => item.user_id == currentUserId) != -1 //在任何人的情况下，谁完成谁才能撤回
+      }
+    } else {
+      compare_user_arr = assignees
+    }
+    for (let val of compare_user_arr) {
+      if (val['user_id'] == currentUserId) {
         currentUserCanOperate = true
         break
       }
@@ -234,7 +245,7 @@ export default class DetailConfirmInfoOne extends React.Component {
             }}>
               {timestampToTimeNormal(deadline, '/', true) || '设置截止时间'}
               {
-                (Number(sort) >= Number(curr_node_sort)) && (
+                (Number(sort) >= Number(curr_node_sort)) && currentUserCanOperate && (
                   <DatePicker onChange={this.datePickerChange.bind(this)}
                     onOpenChange={this.datePikerOnOpenChange.bind(this)}
                     placeholder={'选择截止时间'}
@@ -266,12 +277,12 @@ export default class DetailConfirmInfoOne extends React.Component {
     }
     const filterBottOperate = () => {
       let container = (<div></div>)
-      if ((currentUserCanOperate || assignee_type === '1') && status !== '3') {
+      if ((currentUserCanOperate) && status !== '3') {
         if (Number(sort) < Number(curr_node_sort)) {
           if (Number(curr_node_sort) - Number(sort) === 1) { //相邻才能有撤回
             container = (
               <div>
-                {enable_revocation === '1' ? (
+                {enable_revocation === '1' && currentUserCanReback? (
                   <div className={indexStyles.ConfirmInfoOut_1_bott_right_operate}>
                     <Button onClick={this.setOpinionModalVisible.bind(this, '0')} style={{ color: 'red' }}>撤回</Button>
                   </div>
