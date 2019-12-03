e272860bcade9c1f4f3688f8eaa013a92b2427a7
1109567482@qq.com <cxw2385>
2019-12-03 17:20:51 +0800

    设置已读逻辑修复，前后倒换

--- src/models/technological/cooperationPush/imCooperation.js
+++ src/models/technological/cooperationPush/imCooperation.js
@@ -76,17 +76,6 @@ export default {
             const { messages = [] } = payload
             console.log('ssss_已读列表_model', messages)
             let im_all_latest_unread_messages = yield select(getModelSelectState('imCooperation', 'im_all_latest_unread_messages'))
-            im_all_latest_unread_messages = im_all_latest_unread_messages.filter(item => {
-                if (messages.findIndex((item2) => item2 == item.idServer) == -1) { //传递进来的已读列表不包含该条未读消息
-                    return item
-                }
-            })
-            yield put({
-                type: 'updateDatas',
-                payload: {
-                    im_all_latest_unread_messages
-                }
-            })
             const arr = im_all_latest_unread_messages.filter(item => {
                 if (messages.findIndex((item2) => item2 == item.idServer) != -1) { //传递进来的已读列表不包含该条未读消息
                     return item
@@ -97,6 +86,17 @@ export default {
                 type: 'imMessageToRead',  //sad
                 payload: {
                     reads
+                }
+            })
+            im_all_latest_unread_messages = im_all_latest_unread_messages.filter(item => {
+                if (messages.findIndex((item2) => item2 == item.idServer) == -1) { //传递进来的已读列表不包含该条未读消息
+                    return item
+                }
+            })
+            yield put({
+                type: 'updateDatas',
+                payload: {
+                    im_all_latest_unread_messages
                 }
             })
         },
