92259618765c34883b671762564f7f5e8fbd81b1
1109567482@qq.com <cxw2385>
2020-01-02 17:50:52 +0800

    将model提取到一起

--- src/index.js
+++ src/index.js
@@ -36,7 +36,44 @@ const app = dva({
 // app.use({});
 
 // 3. Model
-app.model(require('./models/workspace-global').default);
+app.model(require('./models/technological/workbench/workbenchTaskDetail').default);
+app.model(require('./models/technological/workbench/workbenchPublicDatas').default);
+app.model(require('./models/technological/workbench/workbenchFileDetail').default);
+app.model(require('./models/technological/workbench/workbenchEditTeamShow').default);
+app.model(require('./models/technological/workbench/workbenchProccessDetail').default);
+app.model(require('./models/technological/cooperationPush').default);
+app.model(require('./models/technological/cooperationPush/imCooperation').default)
+app.model(require('./models/technological/cooperationPush/simpleModeCooperate').default)
+app.model(require('./models/technological/uploadNormal').default)
+app.model(require('./models/technological/globalSearch').default)
+app.model(require('./models/technological/workbench/gantt').default)
+app.model(require('./models/technological/public/milestoneDetail').default)
+app.model(require('./models/technological/public/publicModalComment').default)
+app.model(require('./models/technological/public/publicTaskDetailModal').default)
+app.model(require('./models/technological/public/publicFileDetailModal').default)
+app.model(require('./models/technological/workbench/investmentMap').default)
+app.model(require('./models/technological/accountSet').default)
+app.model(require('./models/technological/project').default)
+app.model(require('./models/technological/projectDetail/index').default)
+app.model(require('./models/technological/projectDetail/projectDetailTask').default)
+app.model(require('./models/technological/projectDetail/projectDetailFile').default)
+app.model(require('./models/technological/projectDetail/projectDetailProcess').default)
+app.model(require('./models/technological/newsDynamic').default)
+app.model(require('./models/technological/workbench/index').default)
+app.model(require('./models/technological/organizationMember').default)
+app.model(require('./models/modal').default)
+app.model(require('./models/teamShow').default)
+app.model(require('./models/teamShow/editTeamShow').default)
+app.model(require('./models/teamShow/teamList').default)
+app.model(require('./models/teamShow/teamInfo').default)
+app.model(require('./models/technological/xczNews').default)
+app.model(require('./models/simpleMode').default)
+app.model(require('./models/simpleMode/simpleWorkbenchbox').default)
+app.model(require('./models/simpleMode/simpleBoardCommunication').default)
+app.model(require('./models/simpleMode/projectCommunication').default)
+app.model(require('./models/organizationManager').default)
+app.model(require('./models/technological/informRemind').default)
+app.model(require('./models/technological').default)
 
 // 4. Router
 app.router(require('./router').default);
