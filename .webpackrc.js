2e50ea60e11585eb37937756daa7d5c61d188d40
1109567482@qq.com <cxw2385>
2020-01-07 18:17:51 +0800

    production环境去掉console

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,9 +1,16 @@
 const path = require('path')
+const NODE_ENV = process.env.NODE_ENV
 
+let plugins = []
+if ('production' == NODE_ENV) {
+  plugins = [
+    "transform-remove-console"
+  ]
+}
 export default {
   "extraBabelPlugins": [
     ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": "css" }],
-    "transform-remove-console"
+    ...plugins
   ],
   alias: {
     '@': path.resolve(__dirname, './src')
