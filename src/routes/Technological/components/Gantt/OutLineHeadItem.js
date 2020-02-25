5d189570a26f36a2908713ce8c3e2a9a666193de
neil <robbie528>
2020-02-25 19:00:41 +0800

    大纲左侧UI

--- src/routes/Technological/components/Gantt/OutLineHeadItem.js
+++ src/routes/Technological/components/Gantt/OutLineHeadItem.js
@@ -10,46 +10,47 @@ export default class OutLineHeadItem extends Component {
         return (
             <div className={styles.out_line_wrapper}>
                 <Tree
-                    showLine
+                    showLine={true}
+                    showIcon={true}
                     switcherIcon={<Icon type="down" />}
                     defaultExpandedKeys={['0-0-0']}
                     onSelect={this.onSelect}
                     blockNode
                 >
-                    <TreeNode title="前期调研" key="0-0">
+                    <TreeNode icon={<span className={styles.milestoneNode}></span>} title="前期调研" key="0-0">
                         <TreeNode title="现场勘探调查" key="0-0-0">
-                            <TreeNode title="交通勘探" key="0-0-0-0" />
-                            <TreeNode title="人文勘探" key="0-0-0-1" />
-                            <TreeNode title={<span className={styles.addTask}>新建子任务</span>} key="0-0-0-2" />
+                            <TreeNode switcherIcon={<span></span>} title="交通勘探" key="0-0-0-0" />
+                            <TreeNode switcherIcon={<span></span>} title="人文勘探" key="0-0-0-1" />
+                            <TreeNode switcherIcon={<span></span>} title={<span className={styles.addTask}>新建子任务</span>} key="0-0-0-2" />
                         </TreeNode>
                         <TreeNode title="座谈会" key="0-0-1">
-                            <TreeNode title="第一次座谈会" key="0-0-1-0" />
-                            <TreeNode title={<span className={styles.addTask}>新建子任务</span>} key="0-0-1-1" />
+                            <TreeNode switcherIcon={<span></span>} title="第一次座谈会" key="0-0-1-0" />
+                            <TreeNode switcherIcon={<span></span>} title={<span className={styles.addTask}>新建子任务</span>} key="0-0-1-1" />
                         </TreeNode>
                         <TreeNode title="问卷调查" key="0-0-2">
-                            <TreeNode title="第一次问卷调查" key="0-0-2-0" />
-                            <TreeNode title={<span className={styles.addTask}>新建子任务</span>} key="0-0-2-1" />
+                            <TreeNode switcherIcon={<span></span>} title="第一次问卷调查" key="0-0-2-0" />
+                            <TreeNode switcherIcon={<span></span>} title={<span className={styles.addTask}>新建子任务</span>} key="0-0-2-1" />
                         </TreeNode>
-                        <TreeNode title="走访访谈" key="0-0-3">
+                        <TreeNode switcherIcon={<span></span>} title="走访访谈" key="0-0-3">
                         </TreeNode>
-                        <TreeNode title="文献资料整理" key="0-0-4">
+                        <TreeNode switcherIcon={<span></span>} title="文献资料整理" key="0-0-4">
                         </TreeNode>
-                        <TreeNode title={<span className={styles.addTask}>新建任务</span>} key="0-0-5">
+                        <TreeNode switcherIcon={<span></span>} title={<span className={styles.addTask}>新建任务</span>} key="0-0-5">
                         </TreeNode>
                     </TreeNode>
-                    <TreeNode title="方案设计" key="0-1">
+                    <TreeNode switcherIcon={<span></span>} title="方案设计" key="0-1">
 
                     </TreeNode>
-                    <TreeNode title="方案设计" key="0-2">
+                    <TreeNode switcherIcon={<span></span>} title="方案设计" key="0-2">
 
                     </TreeNode>
-                    <TreeNode title="成果编制" key="0-3">
+                    <TreeNode switcherIcon={<span></span>} title="成果编制" key="0-3">
 
                     </TreeNode>
-                    <TreeNode title="报审报批交付" key="0-4">
+                    <TreeNode switcherIcon={<span></span>} title="报审报批交付" key="0-4">
 
                     </TreeNode>
-                    <TreeNode title={<span className={styles.addMilestone}>新建里程碑</span>} key="0-5">
+                    <TreeNode switcherIcon={<span></span>} title={<span className={styles.addMilestone}>新建里程碑</span>} key="0-5">
 
                     </TreeNode>
                 </Tree>
