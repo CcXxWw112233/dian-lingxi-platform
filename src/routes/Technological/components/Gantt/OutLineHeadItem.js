4f946cd73a8d92993f161b12012b04a74f5a97f1
neil <robbie528>
2020-02-27 13:02:08 +0800

    大纲树效果

--- src/routes/Technological/components/Gantt/OutLineHeadItem.js
+++ src/routes/Technological/components/Gantt/OutLineHeadItem.js
@@ -1,17 +1,70 @@
 import React, { Component } from 'react';
-import {Icon } from 'antd';
+import { Icon } from 'antd';
 import styles from './index.less';
 import globalStyles from '@/globalset/css/globalClassName.less';
 
 import OutlineTree from './components/OutlineTree';
 
 const { TreeNode, } = OutlineTree;
+
+let demoData = [
+    {
+        id: 100, "name": '前期调研', type: 'milestone', open: true, parentId: 0,
+        children: [
+            {
+                id: 110, "name": '现场勘探调查', type: 'task', open: true, parentId: 100,
+                children: [
+                    { id: 111, "name": '交通勘探', type: 'task', open: true, parentId: 110 },
+                    { id: 112, "name": '人文勘探', type: 'task', open: true, parentId: 110 },
+                ]
+            },
+            {
+                id: 120, "name": '座谈会', type: 'task', open: true, parentId: 100,
+                children: [
+                    { id: 121, "name": '第1次座谈会', type: 'task', open: true, parentId: 120 },
+                    { id: 122, "name": '第2次座谈会', type: 'task', open: true, parentId: 120 },
+                ]
+            },
+            {
+                id: 130, "name": '问卷调查', type: 'task', open: true, parentId: 100,
+                children: [
+                    { id: 131, "name": '第1次问卷调查', type: 'task', open: true, parentId: 130 },
+                    { id: 132, "name": '第2次问卷调查会', type: 'task', open: true, parentId: 130 },
+                ]
+            },
+            {
+                id: 140, "name": '走访访谈', type: 'task', open: true, parentId: 100
+            },
+            {
+                id: 150, "name": '文献资料整理', type: 'task', open: true, parentId: 100
+            }
+        ]
+    },
+    {
+        id: 101, "name": '方案设计', type: 'milestone', open: true, parentId: 0
+    },
+    {
+        id: 102, "name": '方案评估', type: 'milestone', open: true, parentId: 0
+    },
+    {
+        id: 103, "name": '成果编制', type: 'milestone', open: true, parentId: 0
+    },
+    {
+        id: 104, "name": '报审报批交付', type: 'milestone', open: true, parentId: 0
+    }
+]
+
 export default class OutLineHeadItem extends Component {
+    state = {
+        treeData: demoData
+    }
     onSelect = (selectedKeys, info) => {
         console.log('selected', selectedKeys, info);
     };
 
-
+    // onExpand = ({ id, open }) => {
+    //     os
+    // }
     renderGanttOutLineTree = (treeData, isRoot) => {
         if (!treeData) {
             return null;
@@ -25,14 +78,17 @@ export default class OutLineHeadItem extends Component {
                     switcherIcon={<span><Icon type="down" /></span>}
                     // defaultExpandedKeys={['0-0-0']}
                     onSelect={this.onSelect}
+                    // onExpand={this.onExpand}
+
                 >
                     {
                         treeData.map((item, index) => {
                             if (item.children && item.children.length > 0) {
                                 return (
-                                    <TreeNode icon={<span className={`${styles.milestoneNode}`}></span>} title={item.name} key={item.id}>
+                                    <TreeNode  title={item.name} key={item.id}>
                                         {this.renderGanttOutLineTree(item.children)}
                                         <TreeNode
+                                            type={'task'}
                                             icon={<span className={`${styles.addTaskNode} ${globalStyles.authTheme}`}  >&#xe8fe;</span>}
                                             switcherIcon={<span></span>}
                                             title={<span className={styles.addTask}>新建任务</span>} key={`addTask_${item.id}`}>
@@ -42,7 +98,7 @@ export default class OutLineHeadItem extends Component {
                             } else {
                                 return (
 
-                                    <TreeNode switcherIcon={<span></span>} icon={<span className={`${styles.milestoneNode}`}></span>} title={item.name} key={item.id}>
+                                    <TreeNode  title={item.name} key={item.id}>
                                     </TreeNode>
                                 );
                             }
@@ -50,7 +106,7 @@ export default class OutLineHeadItem extends Component {
 
                         })
                     }
-                    <TreeNode icon={<span className={`${styles.addMilestoneNode} ${globalStyles.authTheme}`}  >&#xe8fe;</span>} switcherIcon={<span></span>} title={<span className={styles.addMilestone}>新建里程碑</span>} key="addMilestone">
+                    <TreeNode icon={<span className={`${styles.addMilestoneNode} ${globalStyles.authTheme}`}  >&#xe8fe;</span>}  title={<span className={styles.addMilestone}>新建里程碑</span>} key="addMilestone">
 
                     </TreeNode>
                 </OutlineTree>
@@ -61,30 +117,29 @@ export default class OutLineHeadItem extends Component {
                     if (item.type == 'task') {
                         if (item.children && item.children.length > 0) {
                             return (
-                                <TreeNode icon={<span className={`${styles.taskNode}`}></span>} title={item.name} key={item.id}>
+                                <TreeNode type={item.type} title={item.name} key={item.id}>
                                     {this.renderGanttOutLineTree(item.children)}
                                     <TreeNode
+                                        type={'task'}
                                         icon={<span className={`${styles.addTaskNode} ${globalStyles.authTheme}`}  >&#xe8fe;</span>}
-                                        switcherIcon={<span></span>}
                                         title={<span className={styles.addTask}>新建任务</span>} key={`addTask_${item.id}`}>
                                     </TreeNode>
                                 </TreeNode>
                             );
                         } else {
                             return (
-                                <TreeNode switcherIcon={<span></span>} icon={<span className={`${styles.taskNode}`}></span>} title={item.name} key={item.id}>
-
+                                <TreeNode type={item.type} title={item.name} key={item.id}>
                                 </TreeNode>
                             );
                         }
 
                     } else {
                         return (
-                            <TreeNode icon={<span className={`${styles.milestoneNode}`}></span>} title={item.name} key={item.id}>
+                            <TreeNode type={item.type} title={item.name} key={item.id}>
                                 {this.renderGanttOutLineTree(item.children)}
                                 <TreeNode
+                                    type={'task'}
                                     icon={<span className={`${styles.addTaskNode} ${globalStyles.authTheme}`}  >&#xe8fe;</span>}
-                                    switcherIcon={<span></span>}
                                     title={<span className={styles.addTask}>新建任务</span>} key={`addTask_${item.id}`}>
                                 </TreeNode>
                             </TreeNode>
@@ -101,64 +156,10 @@ export default class OutLineHeadItem extends Component {
     }
 
     render() {
-        const data = [
-            {
-                id: 100, "name": '前期调研', type: 'milestone', open: true, parentId: 0,
-                children: [
-                    {
-                        id: 110, "name": '现场勘探调查', type: 'task', open: true, parentId: 100,
-                        children: [
-                            { id: 111, "name": '交通勘探', type: 'task', open: true, parentId: 110 },
-                            { id: 112, "name": '人文勘探', type: 'task', open: true, parentId: 110 },
-                        ]
-                    },
-                    {
-                        id: 120, "name": '座谈会', type: 'task', open: true, parentId: 100,
-                        children: [
-                            { id: 121, "name": '第1次座谈会', type: 'task', open: true, parentId: 120 },
-                            { id: 122, "name": '第2次座谈会', type: 'task', open: true, parentId: 120 },
-                        ]
-                    },
-                    {
-                        id: 130, "name": '问卷调查', type: 'task', open: true, parentId: 100,
-                        children: [
-                            { id: 131, "name": '第1次问卷调查', type: 'task', open: true, parentId: 130 },
-                            { id: 132, "name": '第2次问卷调查会', type: 'task', open: true, parentId: 130 },
-                        ]
-                    },
-                    {
-                        id: 140, "name": '走访访谈', type: 'task', open: true, parentId: 100
-                    },
-                    {
-                        id: 150, "name": '文献资料整理', type: 'task', open: true, parentId: 100
-                    }
-                ]
-            },
-            {
-                id: 101, "name": '方案设计', type: 'milestone', open: true, parentId: 0
-            },
-            {
-                id: 102, "name": '方案评估', type: 'milestone', open: true, parentId: 0
-            },
-            {
-                id: 103, "name": '成果编制', type: 'milestone', open: true, parentId: 0
-            },
-            {
-                id: 104, "name": '报审报批交付', type: 'milestone', open: true, parentId: 0
-            }
-        ]
+        const { treeData } = this.state;
         return (
             <div className={styles.outline_wrapper}>
-                {/* <OutlineTree /> */}
-                {/* <TreeNode>
-                    <TreeNode/>
-                    <TreeNode/>
-                    <TreeNode/>
-                    <TreeNode/>
-                    <TreeNode/>
-                    <TreeNode/>
-                </TreeNode> */}
-                {this.renderGanttOutLineTree(data, true)}
+                {this.renderGanttOutLineTree(treeData, true)}
             </div>
         );
     }
--- src/routes/Technological/components/Gantt/components/OutlineTree/index.js
+++ src/routes/Technological/components/Gantt/components/OutlineTree/index.js
@@ -1,53 +1,95 @@
 import React, { Component } from 'react'
 import styles from './index.less';
 
+const onExpand = () => {
 
-const TreeNode = (props) => {
-    const { key, leve = 0, isOpen = true, isLeaf = true, title, type = 'milestone' } = props;
-    if (props.children) {
-        console.log(props.children.length);
-        let className = `${styles.outline_tree_node} ${styles[`leve_${leve}`]} ${isLeaf ? (isOpen ? styles.expanded : '') : ''} `;
+}
 
-        return (
-            <div className={className} key={key}>
-                <div className={`${styles.outline_tree_node_content}`} style={{ paddingLeft: (leve * 23) + 'px' }}>
-                    <span className={`${styles.outline_tree_line_node_dot} ${type == 'milestone' ? styles.milestoneNode : ''}`}></span>
-                    {
-                        !isLeaf &&
-                        <span className={`${styles.outline_tree_node_expand_icon} ${isOpen ? styles.expanded : ''}`}></span>
-                    }
-                    <span className={styles.outline_tree_node_label}>{title}</span>
-                </div>
-                <div className={styles.collapse_transition} data-old-padding-top="0px" data-old-padding-bottom="0px" data-old-overflow="hidden" style={{ overflow: 'hidden', paddingTop: '0px', paddingBottom: '0px', display: 'block' }}>
-                    <div className={styles.outline_tree_node_children}>
-                        {
-                            props.children.map((item) => {
-                                return (item);
-                            })
-                        }
-
-                    </div>
-                </div>
-            </div>
-        );
-
-    } else {
-        let className = `${styles.outline_tree_node} ${styles[`leve_${leve}`]} ${isLeaf ? (isOpen ? styles.expanded : '') : ''} `;
-
-        return (
-            <div className={className} key={key}>
-                <div className={`${styles.outline_tree_node_content}`} style={{ paddingLeft: (leve * 23) + 'px' }}>
-                    <span className={`${styles.outline_tree_line_node_dot} ${type == 'milestone' ? styles.milestoneNode : ''}`}></span>
-                    {
-                        !isLeaf &&
-                        <span className={`${styles.outline_tree_node_expand_icon} ${isOpen ? styles.expanded : ''}`}></span>
-                    }
-                    <span className={styles.outline_tree_node_label}>{title}</span>
-                </div>
-            </div>
-        );
+class TreeNode extends Component {
+    constructor(props) {
+        console.log("TreeNode",props);
+        super(props);
+        this.state = {
+            open:true,
+            ...props
+        }
     }
 
+    onChangeExpand = (key, e) => {
+        e.stopPropagation();
+        const open = !this.state.open;
+        this.setState({
+            open
+        });
+        this.props.onExpand&&this.props.onExpand(key, open);
+    }
+
+    render() {
+        const { key, leve = 0, open = true, title, type = 'milestone', icon } = this.state;
+        
+        console.log("openopenopenopen"+key,open);
+        if (this.state.children) {
+            console.log(this.state.children.length);
+            let className = `${styles.outline_tree_node} ${styles[`leve_${leve}`]} ${isLeaf ? (open ? styles.expanded : '') : ''} `;
+            let isLeaf = false;
+            return (
+                <div className={className} key={key}>
+                    <div className={`${styles.outline_tree_node_content}`} style={{ paddingLeft: (leve * 23) + 'px' }}>
+                        <span className={`${styles.outline_tree_line_node_dot} ${type == 'milestone' ? styles.milestoneNode : styles.taskNode}`}></span>
+                        {
+                            !isLeaf &&
+                            <span className={`${styles.outline_tree_node_expand_icon} ${open ? styles.expanded : ''}`} onClick={(e) => { this.onChangeExpand(key, e) }}></span>
+                        }
+                        <span className={styles.outline_tree_node_label}>{title}</span>
+                    </div>
+                    <div className={styles.collapse_transition} data-old-padding-top="0px" data-old-padding-bottom="0px" data-old-overflow="hidden" style={{ overflow: 'hidden', paddingTop: '0px', paddingBottom: '0px', display: open?'block':'none' }}>
+                        <div className={styles.outline_tree_node_children}>
+                            {
+                                React.Children.map(this.state.children, (child, i) => {
+                                    // console.log("child.props", child.props);
+                                    //child.props['leve'] = leve + 1;
+                                    if (child.props.children && child.props.children.length > 0) {
+                                        return (
+                                            <TreeNode {...child.props} leve={leve + 1} isLeaf={false}>
+                                                {child.props.children}
+                                            </TreeNode>
+                                        );
+                                    } else {
+                                        return (
+                                            <TreeNode {...child.props} leve={leve + 1} isLeaf={true} />
+                                        );
+                                    }
+                                })
+                            }
+
+                        </div>
+                    </div>
+                </div>
+            );
+
+        } else {
+            let className = `${styles.outline_tree_node} ${styles[`leve_${leve}`]} ${isLeaf ? (open ? styles.expanded : '') : ''} `;
+            let isLeaf = true;
+            return (
+                <div className={className} key={key}>
+                    <div className={`${styles.outline_tree_node_content}`} style={{ paddingLeft: (leve * 23) + 'px' }}>
+                        {
+                            icon ?
+                                icon
+                                :
+                                <span className={`${styles.outline_tree_line_node_dot} ${type == 'milestone' ? styles.milestoneNode : styles.taskNode}`}></span>
+                        }
+
+                        {
+                            !isLeaf &&
+                            <span className={`${styles.outline_tree_node_expand_icon} ${open ? styles.expanded : ''}`}></span>
+                        }
+                        <span className={styles.outline_tree_node_label}>{title}</span>
+                    </div>
+                </div>
+            );
+        }
+    }
 }
 
 
