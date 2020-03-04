b1c730ebf8decd43f52efdbcf07570256ba59ce0
neil <robbie528>
2020-03-04 17:30:35 +0800

    模板引入确认弹框

--- src/routes/Technological/components/Gantt/components/SafeConfirmModal/index.js
+++ src/routes/Technological/components/Gantt/components/SafeConfirmModal/index.js
@@ -29,15 +29,16 @@ export default class SafeConfirmModal extends Component {
                 onCancel={onChangeVisible}
                 footer={null}
                 destroyOnClose={true}
+                width={437}
             >
-                <div style={{ margin: '12px 24px' }}>
+                <div style={{ margin: '12px 8px' }}>
                     <span style={{ fontSize: '22px', color: '#FAAD14', marginRight: '16px' }}><Icon type="question-circle" theme="filled" /></span>
                     <span style={{ fontSize: '16px', fontWeight: '600', color: ':rgba(0,0,0,0.85)' }}>确认要引用项目模版吗？</span>
                 </div>
-                <div style={{ margin: '12px 24px 12px 50px', color: 'rgba(0,0,0,0.65)' }}>
+                <div style={{ margin: '12px 0px 12px 46px', color: 'rgba(0,0,0,0.65)' }}>
                     引用项目模版会覆盖删除项目现有的数据，如需引用请在下方输入“<span style={{ color: '#1890FF' }}>{confirmContent}</span>”。
                </div>
-                <div style={{ display: 'flex', alignItems: 'center', margin: '12px 24px 12px 50px' }}>
+                <div style={{ display: 'flex', alignItems: 'center', margin: '12px 0px 12px 46px' }}>
                     <div style={{ flex: 'auto' }}><Input style={{ width: '124px' }} placeholder="请输入" value={confirmContentInputValue} onChange={this.onChange} /></div>
                     <div style={{ width: '150px', float: 'right', marginTop: 'right' }}>
                         <Button onClick={this.handleCancel}>
