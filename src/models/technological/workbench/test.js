155c31608a0f70ce7c8eb729784bc19d8e7e68b6
1109567482@qq.com <cxw2385>
2020-09-14 17:37:46 +0800

    递归获取大纲里程碑叶子节点总数，parent_ids正常化

--- src/models/technological/workbench/test.js
+++ src/models/technological/workbench/test.js
@@ -1,13 +1,29 @@
 import { setGantTimeSpan } from '../../../routes/Technological/components/Gantt/ganttBusiness'
 import { getDigit, transformTimestamp } from '../../../utils/util'
 
+function getLeafCountTree(data) {
+    if (data.children.length == 0) {
+        return 1;
+    } else {
+        let leafCount = 1;
+        for (var i = 0; i < data.children.length; i++) {
+            const { parent_expand } = data.children[i]
+            if (parent_expand) {
+                leafCount = leafCount + getLeafCountTree(data.children[i]);
+            } else {
 
-export function recusionItem(tree, { parent_expand, parent_type, parent_id, parent_milestone_id, parent_card_id }, { start_date, end_date }) {
+            }
+        }
+        return leafCount;
+    }
+}
+
+export function recusionItem(tree, { parent_expand, parent_type, parent_id, parent_milestone_id, parent_card_id, parent_ids = [] }, { start_date, end_date }) {
     return tree.map(item => {
         let new_item = { ...item, parent_expand, }
         let { tree_type, children = [], is_expand, id } = item
         let new_item_children = [...children].filter(item => item.id || (item.add_id && item.editing)) //一般项和正在编辑的输入框占位
-        let child_expand_length = 0 //第一级父节点下所有子孙元素展开的总长
+        // let child_expand_length = 0 //第一级父节点下所有子孙元素展开的总长
         let added = new_item_children.find(item => item.tree_type == '0') //表示是否已经添加过虚拟节点
         if ((tree_type == '1' || tree_type == '2') && !added) { //是里程碑或者一级任务,并且没有添加过
             // new_item_children.push({ ...visual_add_item, add_id: item.id }) //添加虚拟节点
@@ -40,7 +56,7 @@ export function recusionItem(tree, { parent_expand, parent_type, parent_id, pare
             time_span = setGantTimeSpan({ time_span, start_time, due_time, start_date, end_date })
         }
         new_item.time_span = time_span
-        new_item.parent_ids = [parent_id]
+        new_item.parent_ids = [].concat(parent_ids, [parent_id]).filter(item => !!item)
         new_item.parent_id = parent_id
         new_item.parent_type = parent_type
         if (parent_type == '1') {
@@ -53,14 +69,17 @@ export function recusionItem(tree, { parent_expand, parent_type, parent_id, pare
             new_item.children = recusionItem(
                 new_item_children,
                 {
-                    parent_expand: is_expand,
                     parent_type: tree_type,
                     parent_expand: is_expand,
                     parent_id: id,
                     parent_milestone_id: id,
-                    parent_card_id: id
+                    parent_card_id: id,
+                    parent_ids: new_item.parent_ids
                 }, { start_date, end_date }
             )
+        }
+        if (tree_type == '1') {
+            new_item.expand_length = getLeafCountTree(new_item)
         }
         return new_item
     })
