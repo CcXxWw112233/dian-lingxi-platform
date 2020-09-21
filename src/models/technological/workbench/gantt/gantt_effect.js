e5cb9a19968c4badd12aef387112e3b2a579fd02
ljf <ljf@qq.com>
2020-09-21 18:36:28 +0800

    '任务详情中依赖关系撤回部分'

--- src/models/technological/workbench/gantt/gantt_effect.js
+++ src/models/technological/workbench/gantt/gantt_effect.js
@@ -244,6 +244,26 @@ export default {
       // ]
 
       const { datas = [], origin_list_group } = payload
+      // 获取影响数据
+      const influenceDatas = datas.scope_content ? datas.scope_content : datas
+      // 获取依赖数据
+      const dependencyDatas = datas.scope_dependency || {}
+      if (!!(dependencyDatas && Object.keys(dependencyDatas).length)) {
+        // 表示如果依赖项存在, 则 表示需要删除依赖
+        const move_id = dependencyDatas.id || ''
+        const line_id =
+          dependencyDatas['next'] &&
+          dependencyDatas['next'][0] &&
+          dependencyDatas['next'][0].id
+        if (!move_id || !line_id) return
+        yield put({
+          type: 'deleteCardRely',
+          payload: {
+            move_id,
+            line_id
+          }
+        })
+      }
       const list_group = yield select(
         getModelSelectDatasState('gantt', 'list_group')
       )
@@ -259,7 +279,7 @@ export default {
           resolve(Aa.then())
         })
       list_group_new = yield call(transform_list_group) || list_group_new
-      for (let val of datas) {
+      for (let val of influenceDatas) {
         const has_time = is_schedule(val.start_time, val.due_time) //存在时间
         for (let val2 of list_group_new) {
           const card_index = val2.lane_data.cards.findIndex(
@@ -397,6 +417,26 @@ export default {
 
     *updateOutLineTree({ payload = {} }, { select, call, put }) {
       const { datas = [] } = payload
+      // 获取影响数据
+      const influenceDatas = datas.scope_content ? datas.scope_content : datas
+      // 获取依赖数据
+      const dependencyDatas = datas.scope_dependency || {}
+      if (!!(dependencyDatas && Object.keys(dependencyDatas).length)) {
+        // 表示如果依赖项存在, 则 表示需要删除依赖
+        const move_id = dependencyDatas.id || ''
+        const line_id =
+          dependencyDatas['next'] &&
+          dependencyDatas['next'][0] &&
+          dependencyDatas['next'][0].id
+        if (!move_id || !line_id) return
+        yield put({
+          type: 'deleteCardRely',
+          payload: {
+            move_id,
+            line_id
+          }
+        })
+      }
       let outline_tree = yield select(
         getModelSelectDatasState('gantt', 'outline_tree')
       )
@@ -409,7 +449,7 @@ export default {
         // if (!data.start_time) nodeValue['start_time'] = ''
         // if (!data.due_time) nodeValue['due_time'] = ''
       }
-      for (let val of datas) {
+      for (let val of influenceDatas) {
         const nodeValue = OutlineTree.getTreeNodeValueByName(
           outline_tree_,
           'id',
