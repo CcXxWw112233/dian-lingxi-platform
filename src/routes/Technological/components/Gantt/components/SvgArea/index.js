fbeb7086759a009f5f3d8388be75c77e16a33a63
1109567482@qq.com <cxw2385>
2020-09-25 09:53:26 +0800

    大纲视图下不显示依赖

--- src/routes/Technological/components/Gantt/components/SvgArea/index.js
+++ src/routes/Technological/components/Gantt/components/SvgArea/index.js
@@ -716,7 +716,7 @@ export default class index extends Component {
   setSVGHeight = () => {
     const rows = 7
     const { ceiHeight, group_view_type, outline_tree_round = [] } = this.props
-    return '100%'
+    // return '100%'
     if (ganttIsOutlineView({ group_view_type })) {
       const outline_tree_round_length = outline_tree_round.length
       if (outline_tree_round_length > rows) {
