b5152b12102ec01b5a0718dbc901015fb9a3a7ca
hejt <hejt@new-di.com>
2020-10-17 11:34:32 +0800

    修复bug

--- src/components/pdfComment/component/colorChoose.js
+++ src/components/pdfComment/component/colorChoose.js
@@ -32,39 +32,40 @@ function colorRGB2Hex(color) {
 export default function ChooseColor(props) {
   const [activeColor, setColor] = useState(props.color)
   let colors = [
-    'rgba(226, 229, 236, 1)',
-    'rgba(255, 233, 72, 1)',
-    'rgba(255, 193, 32, 1)',
-    'rgba(255, 135, 59, 1)',
-    'rgba(158, 166, 194, 1)',
-    'rgba(175, 255, 136, 1)',
-    'rgba(80, 164, 39, 1)',
-    'rgba(255, 78, 59, 1)',
-    'rgba(71, 74, 91, 1)',
-    'rgba(106, 154, 255, 1)',
-    'rgba(51, 112, 21, 1)',
-    'rgba(144, 19, 254, 1)',
-    'rgba(0, 0, 0, 1)',
-    'rgba(65, 112, 232, 1)'
+    '#E2E5EC',
+    '#FFE948',
+    '#FFC120',
+    '#FF873B',
+    '#9EA6C2',
+    '#AFFF88',
+    '#50A427',
+    '#FF4E3B',
+    '#474A5B',
+    '#6A9AFF',
+    '#337015',
+    '#9013FE',
+    '#000000',
+    '#4170E8'
   ]
   useMemo(() => {
-    if (
-      !props.needHex &&
-      props.color &&
-      props.color.indexOf('#') !== -1 &&
-      props.color.length === 7
-    ) {
-      setColor(hexToRgba(props.color, 1).rgba)
-    } else if (props.color) {
-      setColor(props.color)
-    }
+    // if (
+    //   !props.needHex &&
+    //   props.color &&
+    //   props.color.indexOf('#') !== -1 &&
+    //   props.color.length === 7
+    // ) {
+    //   setColor(hexToRgba(props.color, 1).rgba)
+    // } else if (props.color) {
+    //   setColor(props.color)
+    // }
+    setColor(props.color)
   }, [props.color])
   const setActiveColor = val => {
     // setColor(val)
-    console.log(props)
-    if (props.needHex) {
-      props.onChange && props.onChange(val, val)
-    } else props.onChange && props.onChange(val, colorRGB2Hex(val))
+    // console.log(props)
+    // if (props.needHex) {
+    props.onChange && props.onChange(val, val)
+    // } else props.onChange && props.onChange(val, colorRGB2Hex(val))
   }
   const defaultColor = props.colors || colors
   return (
