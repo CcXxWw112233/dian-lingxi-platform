0703b6c101c879d99f84366dc72479de12ef587a
1109567482@qq.com <cxw2385>
2020-11-12 16:54:53 +0800

    去掉分析包

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+//   .BundleAnalyzerPlugin
 const NODE_ENV = process.env.NODE_ENV
 let plugins = []
 if ('production' == NODE_ENV) {
-  plugins = [new BundleAnalyzerPlugin({ analyzerPort: 8333 })]
+  // plugins = [new BundleAnalyzerPlugin({ analyzerPort: 8333 })]
 }
 module.exports = (config, { webpack }) => {
   for (let index = 0; index < plugins.length; index++) {
