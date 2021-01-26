27d8d4dfeb8fde0e8b2e13b602af431c7c6f7a81
ljf <ljf@qq.com>
2021-01-26 11:37:59 +0800

    '项目信息名称缩略'

--- src/routes/SimpleMode/Components/Home/HomeMain/BoardItem.js
+++ src/routes/SimpleMode/Components/Home/HomeMain/BoardItem.js
@@ -716,7 +716,7 @@ export default class BoardItem extends Component {
           >
             <p
               title={board_name}
-              className={`${styles.board_area_middle_item_board_name}`}
+              className={`${styles.board_area_middle_item_board_name} ${globalStyles.global_ellipsis}`}
             >
               {board_name}
             </p>
