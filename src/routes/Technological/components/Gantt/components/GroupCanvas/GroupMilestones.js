74fb66e0d0ac571b2508d8cd603cc9e2127d089b
ljf <ljf@qq.com>
2021-02-08 09:59:21 +0800

    不管分组和项目视图都需要过滤同组里程碑数据

--- src/routes/Technological/components/Gantt/components/GroupCanvas/GroupMilestones.js
+++ src/routes/Technological/components/Gantt/components/GroupCanvas/GroupMilestones.js
@@ -256,15 +256,15 @@ export default class GroupMilestones extends Component {
       t => t.timestamp == timestamp
     )
     let times_arr = Object.keys(milestoneMap) //[timestamp1, timestamp2,...]
-    if (gantt_board_id == '0') {
-      //以分组划分，过滤掉不属于该项目分组的里程碑所属于的时间
-      times_arr = times_arr.filter(
-        time =>
-          milestoneMap[time].findIndex(
-            item => item.board_id == belong_group_id
-          ) != -1
-      )
-    }
+    // if (gantt_board_id == '0') {
+    //以分组划分，过滤掉不属于该项目分组的里程碑所属于的时间
+    times_arr = times_arr.filter(
+      time =>
+        milestoneMap[time].findIndex(
+          item => item.board_id == belong_group_id
+        ) != -1
+    )
+    // }
     times_arr = times_arr.sort((a, b) => Number(a) - Number(b))
     const index = times_arr.findIndex(item => isSamDay(item, timestamp)) //对应上当前日期所属的下标
     const next_miletones_time = times_arr[index + 1] //当前里程碑日期的对应下一个里程碑日期所在时间
