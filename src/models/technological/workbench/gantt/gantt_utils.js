3bf3210448e15cb691d2ef16eb8a1432f49dc439
1109567482@qq.com <cxw2385>
2021-05-13 09:54:32 +0800

    '节点的进度和完成状态受子节点控制'

--- src/models/technological/workbench/gantt/gantt_utils.js
+++ src/models/technological/workbench/gantt/gantt_utils.js
@@ -90,6 +90,15 @@ function getMilestoneLeafWithCompleteCardsTimesPercentage(node) {
   return { complete_time_diff, all_time_diff }
 }
 
+// 获取下级子节点是否全部完成，只有全部完成，该节点才能算完成
+function getChildrenAllRealize(child = []) {
+  let is_realize = '1' //默认完成
+  for (let val of child) {
+    if (val.is_realize == '0') is_realize = '0'
+    break
+  }
+  return is_realize
+}
 // 过滤大纲的隐藏项
 const filterOutlineData = ({ outline_tree_filter_type = {}, arr }) => {
   const {
@@ -319,14 +328,11 @@ export function recusionItem(
       } = getMilestoneLeafWithCompleteCardsTimesPercentage(new_item)
       new_item.start_time = min_leaf_card_time
       new_item.min_leaf_card_time = min_leaf_card_time
-      // if (new_item.is_realize == '1') {
-      //   new_item.percent_card_non = '100.00'
-      // } else {
       new_item.percent_card_non =
         complete_time_diff || all_time_diff
           ? (parseFloat(complete_time_diff / all_time_diff) * 100).toFixed(2)
           : new_item.progress_percent || 0
-      // }
+      new_item.is_realize = getChildrenAllRealize(new_item.children)
     }
     //一级里程碑展开的包含高度
     if (tree_type == '1' && !parent_id) {
