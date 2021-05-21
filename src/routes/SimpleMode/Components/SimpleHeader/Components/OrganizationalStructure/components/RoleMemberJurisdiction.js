ebad93ce6c97d9ec329dcffddd9209c3057e47fe
liu <liuqing_work@163.com>
2021-05-21 10:51:21 +0800

    成员移动至

--- src/routes/SimpleMode/Components/SimpleHeader/Components/OrganizationalStructure/components/RoleMemberJurisdiction.js
+++ src/routes/SimpleMode/Components/SimpleHeader/Components/OrganizationalStructure/components/RoleMemberJurisdiction.js
@@ -6,56 +6,63 @@ import { OrgStructureModel } from '../../../../../../../models/technological/org
 import { Tree } from 'antd'
 const getEffectOrReducerByName = name => `organizationManager/${name}`
 
-@connect(({ [OrgStructureModel.namespace]: { orgPermissionsList,currentPermissionList } }) => ({
-  orgPermissionsList,currentPermissionList
-}))
+@connect(
+  ({
+    [OrgStructureModel.namespace]: { orgPermissionsList, currentPermissionList }
+  }) => ({
+    orgPermissionsList,
+    currentPermissionList
+  })
+)
 /** 组织架构的右侧角色权限列表
  * @description 用于展示组织架构权限列表
  */
 export default class RoleMemberJurisdiction extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-    }
+    this.state = {}
   }
   /**
    * 选择权限 key 参数
    */
-  selectJurisdiction = key =>{
-      console.log('选择权限',key)
+  selectJurisdiction = key => {
+    console.log('选择权限', key)
   }
   /**
    * 保存用户权限
    * @param {*} checkedKeys  权限Id
-   * @param {*} e 
+   * @param {*} e
    */
-  onCheck(checkedKeys,e) {
+  onCheck(checkedKeys, e) {
     console.log(checkedKeys.checked)
-    
-    const {dispatch,role_id} = this.props;
+
+    const { dispatch, role_id } = this.props
     var data = {
-      box_type_ids: "",
+      box_type_ids: '',
       function_data: checkedKeys.checked,
       role_id: role_id
     }
     dispatch({
-        type: [
-          OrgStructureModel.namespace,
-          'savePermission'
-        ].join('/'),
-        payload: data
+      type: [OrgStructureModel.namespace, 'savePermission'].join('/'),
+      payload: data
     })
   }
 
-
   render() {
-    const {orgPermissionsList = [],currentPermissionList,canHandle} = this.props
-    const { TreeNode } = Tree;
-    return  <div className={styles.role_Jurisdiction} style={{
-      overflowY: 'auto',
-      position: 'relative',
-    }}>
-
+    const {
+      orgPermissionsList = [],
+      currentPermissionList,
+      canHandle
+    } = this.props
+    const { TreeNode } = Tree
+    return (
+      <div
+        className={styles.role_Jurisdiction}
+        style={{
+          overflowY: 'auto',
+          position: 'relative'
+        }}
+      >
         {/* {
             jurisdictionList.map((item,key) => {
                 return <div className={styles.role_Jurisdiction_contant}>
@@ -72,29 +79,44 @@ export default class RoleMemberJurisdiction extends React.Component {
             })
         } */}
         <Tree
-        checkable
-        checkStrictly={true}
-        // defaultExpandedKeys={[]}
-        defaultSelectedKeys={currentPermissionList}
-        defaultCheckedKeys={currentPermissionList}
-        onSelect={this.onSelect}
-        onCheck={this.onCheck.bind(this)}
-      >
-        {
-          orgPermissionsList.map((item,key)=>{
-            const child_data =  item['child_data']|| []
-            return <TreeNode disableCheckbox title={<span className={`${styles.role_Jurisdiction_title}`}>{item.name}</span>} key={item.id+'_'+key} >
-                {
-                  child_data.map((value,key)=>{
-                  return  <TreeNode disableCheckbox={!canHandle} title={<span className={`${styles.role_Jurisdiction_title}`}>{value.name}</span>} key={value.id}> 
-                    </TreeNode>
-                  })
+          checkable
+          checkStrictly={true}
+          // defaultExpandedKeys={[]}
+          defaultSelectedKeys={currentPermissionList}
+          defaultCheckedKeys={currentPermissionList}
+          onSelect={this.onSelect}
+          onCheck={this.onCheck.bind(this)}
+        >
+          {orgPermissionsList.map((item, key) => {
+            const child_data = item['child_data'] || []
+            return (
+              <TreeNode
+                disableCheckbox
+                title={
+                  <span className={`${styles.role_Jurisdiction_title}`}>
+                    {item.name}
+                  </span>
                 }
-          </TreeNode>
-          })
-        }
-      </Tree>
-     
-    </div>
+                key={item.id + '_' + key}
+              >
+                {child_data.map((value, key) => {
+                  return (
+                    <TreeNode
+                      disableCheckbox={!canHandle}
+                      title={
+                        <span className={`${styles.role_Jurisdiction_title}`}>
+                          {value.name}
+                        </span>
+                      }
+                      key={value.id}
+                    ></TreeNode>
+                  )
+                })}
+              </TreeNode>
+            )
+          })}
+        </Tree>
+      </div>
+    )
   }
 }
